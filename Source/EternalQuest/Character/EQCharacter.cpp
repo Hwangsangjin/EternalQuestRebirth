aedfbede9d18a97a4122105174ff158797e0c9e8
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-02 00:53:47 +0900

    캐릭터 클래스 추가

---
+++ Source/EternalQuest/Character/EQCharacter.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQCharacter.h"
+
+AEQCharacter::AEQCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AEQCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AEQCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEQCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/EternalQuest/Character/EQCharacter.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EQCharacter.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEQCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+};
