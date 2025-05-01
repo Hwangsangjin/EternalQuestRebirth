99bee2e401a84798676840b11b914de081efde38
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-02 00:54:28 +0900

    플레이어 컨트롤러 클래스 추가

---
+++ Source/EternalQuest/Player/EQPlayerController.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EQPlayerController.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
