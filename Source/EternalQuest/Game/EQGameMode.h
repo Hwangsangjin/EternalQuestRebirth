2e76b7e03343590dd4e8466b97cadeb32956639b
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-02 00:53:37 +0900

    게임 모드 클래스 추가

---
+++ Source/EternalQuest/Game/EQGameMode.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EQGameMode.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
