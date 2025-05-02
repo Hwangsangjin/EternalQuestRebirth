c406728b49f54c2fb1f96daf72c56a892ab6b618
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-02 23:56:06 +0900

    애님인스턴스 클래스 추가

---
+++ Source/EternalQuest/Animation/EQAnimInstance.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UEQAnimInstance::UEQAnimInstance()
+{
+	MovingThreshold = 3.0f;
+}
+
+void UEQAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (IsValid(Owner))
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void UEQAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (IsValid(Movement))
+	{
+		Velocity = Movement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+
+		bIsMoving = GroundSpeed > MovingThreshold && Movement->GetCurrentAcceleration() != FVector::ZeroVector;
+
+		bIsFalling = Movement->IsFalling();
+	}
+}
---
+++ Source/EternalQuest/Animation/EQAnimInstance.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EQAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UEQAnimInstance();
+
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	TObjectPtr<ACharacter> Owner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UCharacterMovementComponent> Movement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	uint8 bIsMoving : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	float MovingThreshold;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = true))
+	uint8 bIsFalling : 1;
+};
