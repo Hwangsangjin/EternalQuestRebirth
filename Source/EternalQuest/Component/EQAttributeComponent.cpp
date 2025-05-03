2d32e37084436a6b9000aca3a24270f4028d606a
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-03 23:24:55 +0900

    속성 컴포넌트 추가

---
+++ Source/EternalQuest/Component/EQAttributeComponent.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAttributeComponent.h"
+
+UEQAttributeComponent::UEQAttributeComponent()
+{
+	RegainRate = 0.1f;
+	MaxStamina = 100.0f;
+	BaseStamina = MaxStamina;
+}
+
+bool UEQAttributeComponent::CheckHasEnoughStamina(float StaminaCost) const
+{
+	return BaseStamina >= StaminaCost;
+}
+
+void UEQAttributeComponent::DecreaseStamina(float StaminaCost)
+{
+	BaseStamina = FMath::Clamp(BaseStamina - StaminaCost, 0.0f, MaxStamina);
+}
+
+void UEQAttributeComponent::ToggleRegainStamina(bool bEnabled)
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	if (bEnabled)
+	{
+		if (!TimerManager.IsTimerActive(RegainStaminaTimerHandle))
+		{
+			TimerManager.SetTimer(RegainStaminaTimerHandle, this, &ThisClass::RegainStamina, 0.1f, true);
+		}
+	}
+	else
+	{
+		TimerManager.ClearTimer(RegainStaminaTimerHandle);
+	}
+}
+
+void UEQAttributeComponent::RegainStamina()
+{
+	// 스태미나를 RegainRate만큼 회복하되, 최대치를 초과하지 않도록 클램프
+	BaseStamina = FMath::Clamp(BaseStamina + RegainRate, 0.0f, MaxStamina);
+
+	// 스태미나 변경 알림
+	BroadcastAttributeChanged(EEQAttributeType::Stamina);
+
+	// 스태미나가 가득 찼다면 회복 중지
+	if (BaseStamina >= MaxStamina)
+	{
+		ToggleRegainStamina(false);
+	}
+}
+
+void UEQAttributeComponent::BroadcastAttributeChanged(EEQAttributeType InAttributeType) const
+{
+	// 델리게이트가 바인딩되어 있을 경우만 실행
+	if (OnAttributeChanged.IsBound())
+	{
+		float Ratio = 0.0f;
+
+		switch (InAttributeType)
+		{
+		case EEQAttributeType::Stamina:
+			Ratio = GetStaminaRatio();
+			break;
+		}
+
+		// 속성 타입과 비율을 브로드캐스트
+		OnAttributeChanged.Broadcast(InAttributeType, Ratio);
+	}
+}
---
+++ Source/EternalQuest/Component/EQAttributeComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EternalQuestDefine.h"
+#include "Components/ActorComponent.h"
+#include "EQAttributeComponent.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnAttributeChanged, EEQAttributeType, float);
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQAttributeComponent();
+
+	FORCEINLINE float GetBaseStamina() const { return BaseStamina; }
+	FORCEINLINE float GetMaxStamina() const { return MaxStamina; }
+
+	FORCEINLINE float GetStaminaRatio() const { return BaseStamina / MaxStamina; }
+
+	bool CheckHasEnoughStamina(float StaminaCost) const;
+	void DecreaseStamina(float StaminaCost);
+	void ToggleRegainStamina(bool bEnabled);
+	void RegainStamina();
+
+	void BroadcastAttributeChanged(EEQAttributeType InAttributeType) const;
+	FOnAttributeChanged OnAttributeChanged;
+
+private:
+	UPROPERTY(EditAnywhere, Category = Stamina, meta = (AllowPrivateAccess = true))
+	float BaseStamina;
+
+	UPROPERTY(EditAnywhere, Category = Stamina, meta = (AllowPrivateAccess = true))
+	float MaxStamina;
+
+	UPROPERTY(EditAnywhere, Category = Stamina, meta = (AllowPrivateAccess = true))
+	float RegainRate;
+
+	FTimerHandle RegainStaminaTimerHandle;
+};
