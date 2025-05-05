17930b9c299c00ee64592af66be63eff6cca4756
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-05 23:58:22 +0900

    StatBar 위젯 추가

---
+++ Source/EternalQuest/Widget/EQStatBarWidget.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQStatBarWidget.h"
+#include "Components/ProgressBar.h"
+
+UEQStatBarWidget::UEQStatBarWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UEQStatBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ProgressBar == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Progress Bar is nullptr."));
+		return;
+	}
+
+	ProgressBar->SetFillColorAndOpacity(FillColorAndOpacity);
+}
+
+void UEQStatBarWidget::SetProgressBarPercent(float InRatio) const
+{
+	if (ProgressBar == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Progress Bar is nullptr."));
+		return;
+	}
+
+	ProgressBar->SetPercent(InRatio);
+}
