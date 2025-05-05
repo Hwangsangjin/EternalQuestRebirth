5367c52caea12a380dacac4585bfb949c1796823
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-05 23:58:15 +0900

    User 위젯 추가

---
+++ Source/EternalQuest/Widget/EQUserWidget.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EQUserWidget.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	FORCEINLINE AActor* GetOwningActor() const { return OwningActor.Get(); }
+	FORCEINLINE void SetOwningActor(AActor* InActor) { OwningActor = InActor; }
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Actor)
+	TObjectPtr<AActor> OwningActor;
+};
