6f4b77c478c9fe9c8f13bc83f02d798c35ffe1ab
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-05 23:58:30 +0900

    HUD 위젯 추가

---
+++ Source/EternalQuest/Widget/EQPlayerHUDWidget.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQUserWidget.h"
+#include "EternalQuestDefine.h"
+#include "EQPlayerHUDWidget.generated.h"
+
+class UEQStatBarWidget;
+
+UCLASS()
+class ETERNALQUEST_API UEQPlayerHUDWidget : public UEQUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	explicit UEQPlayerHUDWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	void UpdateWidget(EEQAttributeType InAttributeType, float InRatio);
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQStatBarWidget> StaminaBarWidget;
+};
