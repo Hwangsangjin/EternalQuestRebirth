475449b079006fc928a9793b0b6866d6aeba1f4e
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-06 00:53:52 +0900

    스태미나 UI 추가

--- Source/EternalQuest/Player/EQPlayerController.cpp
+++ Source/EternalQuest/Player/EQPlayerController.cpp
@@ -1,3 +1,36 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "EQPlayerController.h"
+#include "Widget/EQPlayerHUDWidget.h"
+
+AEQPlayerController::AEQPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UEQPlayerHUDWidget> PlayerHUDWidgetClassRef(TEXT("/Game/Blueprints/UI/WBP_PlayerHUD.WBP_PlayerHUD_C"));
+	if (PlayerHUDWidgetClassRef.Succeeded())
+	{
+		PlayerHUDWidgetClass = PlayerHUDWidgetClassRef.Class;
+	}
+}
+
+void AEQPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const FInputModeGameOnly InputModeGameOnly;
+	SetInputMode(InputModeGameOnly);
+
+	if (PlayerHUDWidgetClass == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player HUD Widget Class is nullptr."));
+		return;
+	}
+
+	PlayerHUDWidget = CreateWidget<UEQPlayerHUDWidget>(this, PlayerHUDWidgetClass);
+	if (PlayerHUDWidget == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player HUD Widget is nullptr."));
+		return;
+	}
+
+	PlayerHUDWidget->AddToViewport();
+}
--- Source/EternalQuest/Player/EQPlayerController.h
+++ Source/EternalQuest/Player/EQPlayerController.h
@@ -6,8 +6,23 @@
 #include "GameFramework/PlayerController.h"
 #include "EQPlayerController.generated.h"
 
+class UEQPlayerHUDWidget;
+
 UCLASS()
 class ETERNALQUEST_API AEQPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AEQPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = UI, meta = (AllowPrivateAccess = true))
+	TSubclassOf<UUserWidget> PlayerHUDWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQPlayerHUDWidget> PlayerHUDWidget;
 };
