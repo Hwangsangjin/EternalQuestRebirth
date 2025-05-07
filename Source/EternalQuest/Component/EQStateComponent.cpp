19735f2d357b3719fc86b0274df640788ace919c
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-05-07 23:59:27 +0900

    상태 컴포넌트 추가

---
+++ Source/EternalQuest/Component/EQStateComponent.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQStateComponent.h"
+#include "EternalQuestGameplayTags.h"
+
+UEQStateComponent::UEQStateComponent()
+{
+	bMovementInputEnabled = true;
+}
+
+bool UEQStateComponent::IsCurrentStateEqualToAny(const FGameplayTagContainer& GameplayTagContainer) const
+{
+	return GameplayTagContainer.HasTagExact(CurrentState);
+}
+
+void UEQStateComponent::ClearState()
+{
+	if (CurrentState == EternalQuestGameplayTags::Character_State_Death)
+	{
+		return;
+	}
+
+	CurrentState = FGameplayTag::EmptyTag;
+}
---
+++ Source/EternalQuest/Component/EQStateComponent.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "EQStateComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQStateComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQStateComponent();
+
+	bool IsCurrentStateEqualToAny(const FGameplayTagContainer& GameplayTagContainer) const;
+
+	FORCEINLINE FGameplayTag GetCurrentState() { return CurrentState; }
+	FORCEINLINE void SetState(const FGameplayTag& NewState) { CurrentState = NewState; }
+	void ClearState();
+
+	FORCEINLINE bool IsMovementInputEnabled() const { return bMovementInputEnabled; }
+	FORCEINLINE void ToggleMovementInput(bool bEnabled) { bMovementInputEnabled = bEnabled; }
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = State, meta = (AllowPrivateAccess = true))
+	FGameplayTag CurrentState;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = State, meta = (AllowPrivateAccess = true))
+	uint8 bMovementInputEnabled : 1;
+};
