932349d50b339ffcc276e9ddd727aeebf805770c
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-06-26 23:49:33 +0900

    무기 클래스 추가

---
+++ Source/EternalQuest/Equipment/EQWeapon.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQWeapon.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Animation/EQAnimInstance.h"
+#include "Character/EQBaseCharacter.h"
+#include "Component/EQCollisionComponent.h"
+#include "Component/EQCombatComponent.h"
+#include "Data/EQMontageActionData.h"
+#include "Kismet/GameplayStatics.h"
+
+AEQWeapon::AEQWeapon()
+{
+	CollisionComponent = CreateDefaultSubobject<UEQCollisionComponent>(TEXT("CollisionComponent"));
+	CollisionComponent->OnHitDelegate.AddUObject(this, &ThisClass::OnHitCallback);
+	CombatType = ECombatType::None;
+}
+
+void AEQWeapon::EquipItem()
+{
+	Super::EquipItem();
+
+	CombatComponent = GetOwner()->GetComponentByClass<UEQCombatComponent>();
+	if (!CombatComponent)
+	{
+		return;
+	}
+
+	CombatComponent->SetWeapon(this);
+
+	const FName SocketName = CombatComponent->IsCombatEnabled() ? EquipSocketName : UnequipSocketName;
+	AttachToOwner(SocketName);
+
+	const AEQBaseCharacter* OwnerCharacter = Cast<AEQBaseCharacter>(GetOwner());
+	if (!OwnerCharacter)
+	{
+		return;
+	}
+
+	UEQAnimInstance* AnimInstance = Cast<UEQAnimInstance>(OwnerCharacter->GetMesh()->GetAnimInstance());
+	if (!AnimInstance)
+	{
+		return;
+	}
+
+	AnimInstance->UpdateCombatType(CombatType);
+
+	CollisionComponent->SetPrimitiveComponent(SkeletalMeshComponent);
+	CollisionComponent->AddIgnoredActor(GetOwner());
+}
+
+void AEQWeapon::UnequipItem()
+{
+	Super::UnequipItem();
+}
+
+void AEQWeapon::ActivateCollision()
+{
+	CollisionComponent->TurnOnCollision();
+}
+
+void AEQWeapon::DeactivateCollision()
+{
+	CollisionComponent->TurnOffCollision();
+}
+
+UAnimMontage* AEQWeapon::GetMontageForTag(const FGameplayTag& GameplayTag, const int32 Index) const
+{
+	return MontageActionData->GetMontageForTag(GameplayTag, Index);
+}
+
+UAnimMontage* AEQWeapon::GetRandomMontageForTag(const FGameplayTag& GameplayTag) const
+{
+	return MontageActionData->GetRandomMontageForTag(GameplayTag);
+}
+
+void AEQWeapon::OnHitCallback(const FHitResult& HitResult)
+{
+	AActor* TargetActor = HitResult.GetActor();
+
+	const FVector DamageDirection = GetOwner()->GetActorForwardVector();
+
+	constexpr float AttackDamage = 0.0f;
+
+	UGameplayStatics::ApplyPointDamage(TargetActor, AttackDamage, DamageDirection, HitResult, GetOwner()->GetInstigatorController(), this, nullptr);
+}
---
+++ Source/EternalQuest/Equipment/EQWeapon.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQEquipment.h"
+#include "GameplayTagContainer.h"
+#include "EQWeapon.generated.h"
+
+enum class ECombatType : uint8;
+class UEQCollisionComponent;
+class UEQMontageActionData;
+class UEQCombatComponent;
+
+UCLASS()
+class ETERNALQUEST_API AEQWeapon : public AEQEquipment
+{
+	GENERATED_BODY()
+
+public:
+	AEQWeapon();
+
+	virtual void EquipItem() override;
+	virtual void UnequipItem() override;
+
+	virtual void ActivateCollision();
+	virtual void DeactivateCollision();
+
+	UAnimMontage* GetMontageForTag(const FGameplayTag& GameplayTag, const int32 Index = 0) const;
+	UAnimMontage* GetRandomMontageForTag(const FGameplayTag& GameplayTag) const;
+
+	FORCEINLINE FName GetEquipSocketName() const { return EquipSocketName; }
+	FORCEINLINE FName GetUnequipSocketName() const { return UnequipSocketName; }
+
+	FORCEINLINE UEQCollisionComponent* GetCollisionComponent() const { return CollisionComponent; }
+
+	void OnHitCallback(const FHitResult& HitResult);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Socket, Meta = (AllowPrivateAccess = true))
+	FName EquipSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Socket, Meta = (AllowPrivateAccess = true))
+	FName UnequipSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Data, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQMontageActionData> MontageActionData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQCombatComponent> CombatComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQCollisionComponent> CollisionComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Equipment, Meta = (AllowPrivateAccess = true))
+	ECombatType CombatType;
+};
