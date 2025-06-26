76d2e8e04fa79ef086a13c441d82481d4cab6a8d
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-06-26 23:33:56 +0900

    장비 클래스 추가

---
+++ Source/EternalQuest/Equipment/EQEquipment.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQEquipment.h"
+#include "GameFramework/Character.h"
+
+AEQEquipment::AEQEquipment()
+{
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	SetRootComponent(SkeletalMeshComponent);
+}
+
+void AEQEquipment::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (!SkeletalMesh)
+	{
+		return;
+	}
+
+	SkeletalMeshComponent->SetSkeletalMesh(SkeletalMesh);
+}
+
+void AEQEquipment::EquipItem()
+{
+}
+
+void AEQEquipment::UnequipItem()
+{
+}
+
+void AEQEquipment::AttachToOwner(FName SocketName)
+{
+	const ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner());
+	if (!OwnerCharacter)
+	{
+		return;
+	}
+
+	USkeletalMeshComponent* CharacterMeshComponent = OwnerCharacter->GetMesh();
+	if (!CharacterMeshComponent)
+	{
+		return;
+	}
+
+	AttachToComponent(CharacterMeshComponent, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), SocketName);
+}
---
+++ Source/EternalQuest/Equipment/EQEquipment.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EQEquipment.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQEquipment : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEQEquipment();
+
+	virtual void EquipItem();
+	virtual void UnequipItem();
+	virtual void AttachToOwner(FName SocketName);
+
+	FORCEINLINE USkeletalMesh* GetSkeletalMesh() const { return SkeletalMesh; }
+	FORCEINLINE void SetSkeletalMesh(USkeletalMesh* NewSkeletalMesh) { SkeletalMesh = NewSkeletalMesh; }
+
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	UPROPERTY(EditAnywhere, Category = Equipment, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<USkeletalMesh> SkeletalMesh;
+
+	UPROPERTY(EditAnywhere, Category = Equipment, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+};
