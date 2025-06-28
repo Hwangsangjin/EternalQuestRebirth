130924006b4b54b9173f45fab1ed32776a95167f
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-06-28 23:44:39 +0900

    HUD 위젯 클래스 추가

---
+++ Source/EternalQuest/UI/EQHUDWidget.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQHUDWidget.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "EQAttributeBarWidget.h"
+#include "EQButtonWidget.h"
+#include "Components/TextBlock.h"
+
+UEQHUDWidget::UEQHUDWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UEQHUDWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	APawn* OwningPlayerPawn = GetOwningPlayerPawn();
+	if (!OwningPlayerPawn)
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* NewAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OwningPlayerPawn);
+	if (!NewAbilitySystemComponent)
+	{
+		return;
+	}
+
+	check(HealthBarWidget);
+	check(ManaBarWidget);
+	check(StaminaBarWidget);
+	check(LeftButtonWidget);
+
+	HealthBarWidget->SetAbilitySystemComponent(NewAbilitySystemComponent);
+	ManaBarWidget->SetAbilitySystemComponent(NewAbilitySystemComponent);
+	StaminaBarWidget->SetAbilitySystemComponent(NewAbilitySystemComponent);
+	LeftButtonWidget->SetAbilitySystemComponent(NewAbilitySystemComponent);
+
+	check(InteractPromptWidget);
+	check(WarningPromptWidget);
+
+	InteractPromptWidget->SetVisibility(ESlateVisibility::Hidden);
+	WarningPromptWidget->SetVisibility(ESlateVisibility::Hidden);
+}
---
+++ Source/EternalQuest/UI/EQHUDWidget.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQUserWidget.h"
+#include "EQHUDWidget.generated.h"
+
+class UEQButtonWidget;
+class UEQAttributeBarWidget;
+
+UCLASS()
+class ETERNALQUEST_API UEQHUDWidget : public UEQUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	explicit UEQHUDWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	FORCEINLINE UEQUserWidget* GetInteractPromptWidget() const { return InteractPromptWidget; }
+	FORCEINLINE UEQUserWidget* GetWarningPromptWidget() const { return WarningPromptWidget; }
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, Meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQAttributeBarWidget> HealthBarWidget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, Meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQAttributeBarWidget> ManaBarWidget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, Meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQAttributeBarWidget> StaminaBarWidget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, Meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQUserWidget> InteractPromptWidget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, Meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQUserWidget> WarningPromptWidget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = UI, Meta = (BindWidget, AllowPrivateAccess = true))
+	TObjectPtr<UEQButtonWidget> LeftButtonWidget;
+};
