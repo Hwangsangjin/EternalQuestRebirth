d1ecf6b9c89402a5dccf68b16193c28f443131d8
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-06-28 23:43:55 +0900

    버튼 위젯 클래스 추가

---
+++ Source/EternalQuest/UI/EQButtonWidget.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQButtonWidget.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayTag/EQGameplayTag.h"
+
+UEQButtonWidget::UEQButtonWidget()
+{
+}
+
+void UEQButtonWidget::SetAbilitySystemComponent(UAbilitySystemComponent* InAbilitySystemComponent)
+{
+	Super::SetAbilitySystemComponent(InAbilitySystemComponent);
+
+	if (!AbilitySystemComponent)
+	{
+		return;
+	}
+
+	AbilitySystemComponent->RegisterGameplayTagEvent(EQGameplayTag::UI_Notify_ButtonPressed, EGameplayTagEventType::NewOrRemoved).AddUObject(this, &ThisClass::OnButtonPressed);
+}
+
+void UEQButtonWidget::OnButtonPressed(const FGameplayTag Tag, int32 NewCount)
+{
+	check(PressedAnim);
+
+	if (NewCount > 0)
+	{
+		FTimerHandle DelayAnimTimer;
+		GetWorld()->GetTimerManager().SetTimer(DelayAnimTimer, FTimerDelegate::CreateLambda([this]()
+		{
+			PlayAnimation(PressedAnim);
+		}), 0.01f, false);
+	}
+}
---
+++ Source/EternalQuest/UI/EQButtonWidget.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQUserWidget.h"
+#include "EQButtonWidget.generated.h"
+
+struct FGameplayTag;
+
+UCLASS()
+class ETERNALQUEST_API UEQButtonWidget : public UEQUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UEQButtonWidget();
+
+	virtual void SetAbilitySystemComponent(UAbilitySystemComponent* InAbilitySystemComponent) override;
+
+protected:
+	UFUNCTION()
+	void OnButtonPressed(const FGameplayTag Tag, int32 NewCount);
+
+private:
+	UPROPERTY(EditAnywhere, Transient, Meta = (BindWidgetAnim, AllowPrivateAccess = true))
+	UWidgetAnimation* PressedAnim;
+};
