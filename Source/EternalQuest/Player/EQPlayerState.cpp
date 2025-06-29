2faf8ce49bdc31b43d7eef40476a5b48d88ae94a
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-06-30 07:33:31 +0900

    플레이어 스테이트 클래스 추가

---
+++ Source/EternalQuest/Player/EQPlayerState.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQPlayerState.h"
+#include "AbilitySystemComponent.h"
+#include "Attribute/EQCharacterAttributeSet.h"
+#include "Attribute/EQCharacterSkillAttributeSet.h"
+
+AEQPlayerState::AEQPlayerState()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UEQCharacterAttributeSet>(TEXT("AttributeSet"));
+	SkillAttributeSet = CreateDefaultSubobject<UEQCharacterSkillAttributeSet>(TEXT("SkillAttributeSet"));
+}
+
+UAbilitySystemComponent* AEQPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/EternalQuest/Player/EQPlayerState.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "EQPlayerState.generated.h"
+
+class UEQCharacterSkillAttributeSet;
+class UEQCharacterAttributeSet;
+
+UCLASS()
+class ETERNALQUEST_API AEQPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AEQPlayerState();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQCharacterAttributeSet> AttributeSet;
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQCharacterSkillAttributeSet> SkillAttributeSet;
+};
