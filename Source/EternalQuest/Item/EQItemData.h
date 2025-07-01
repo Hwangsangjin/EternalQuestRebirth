5423853d4a977bd3bdc3802b469ef40e28e31e12
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-01 20:45:54 +0900

    아이템 데이터 클래스 추가

---
+++ Source/EternalQuest/Item/EQItemData.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "EQItemData.generated.h"
+
+class AEQEquipment;
+
+UCLASS()
+class ETERNALQUEST_API UEQItemData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = Equipment)
+	TSubclassOf<AEQEquipment> EquipmentClass;
+};
