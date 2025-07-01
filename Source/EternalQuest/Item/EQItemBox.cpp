dbb8615bea4faa7cc9b9fdce67d4852888429991
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-01 20:47:05 +0900

    아이템 박스 클래스 추가

---
+++ Source/EternalQuest/Item/EQItemBox.cpp
@@ -0,0 +1,141 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQItemBox.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "EQPickupItem.h"
+#include "Components/BoxComponent.h"
+#include "GameplayTag/EQGameplayTag.h"
+#include "Physics/EQCollision.h"
+
+AEQItemBox::AEQItemBox()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+
+	TriggerBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBoxComponent"));
+	TriggerBoxComponent->SetCollisionProfileName(CPROFILE_EQTRIGGER);
+	SetRootComponent(TriggerBoxComponent);
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	SkeletalMeshComponent->SetupAttachment(TriggerBoxComponent);
+	SkeletalMeshComponent->SetCollisionProfileName(TEXT("NoCollision"));
+}
+
+UAbilitySystemComponent* AEQItemBox::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AEQItemBox::PerformInteract(AActor* InteractActor)
+{
+	if (!SkeletalMeshComponent)
+	{
+		return;
+	}
+
+	SetActorEnableCollision(false);
+	SkeletalMeshComponent->Play(false);
+
+	ApplyEffectToTarget(InteractActor);
+
+	UAnimationAsset* AnimAsset = SkeletalMeshComponent->AnimationData.AnimToPlay;
+	const UAnimSequence* AnimSequence = Cast<UAnimSequence>(AnimAsset);
+	const float AnimDuration = AnimSequence ? AnimSequence->GetPlayLength() : 1.0f;
+
+	for (UEQItemData* ItemData : ItemDataArray)
+	{
+		if (!ItemData)
+		{
+			continue;
+		}
+
+		FTimerHandle ItemSpawnTimerHandle;
+		GetWorld()->GetTimerManager().SetTimer(ItemSpawnTimerHandle, [this, InteractActor, ItemData]()
+		{
+			InvokeGameplayCue(InteractActor);
+
+			AEQPickupItem* PickupItem = GetWorld()->SpawnActorDeferred<AEQPickupItem>(AEQPickupItem::StaticClass(), GetActorTransform(), nullptr, nullptr, ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn);
+			PickupItem->SetItemData(ItemData);
+			PickupItem->FinishSpawning(GetActorTransform());
+			PickupItem->Throw();
+		}, AnimDuration * 0.75f, false);
+	}
+}
+
+void AEQItemBox::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
+
+void AEQItemBox::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	APawn* OverlappingPawn = Cast<APawn>(OtherActor);
+	if (!OverlappingPawn || !OverlappingPawn->IsLocallyControlled())
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OverlappingPawn);
+	if (!TargetAbilitySystemComponent)
+	{
+		return;
+	}
+
+	FGameplayCueParameters Params;
+	Params.Instigator = OtherActor;
+	TargetAbilitySystemComponent->ExecuteGameplayCue(EQGameplayTag::GameplayCue_InteractPrompt_Show, Params);
+}
+
+void AEQItemBox::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+
+	APawn* OverlappingPawn = Cast<APawn>(OtherActor);
+	if (!OverlappingPawn || !OverlappingPawn->IsLocallyControlled())
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OverlappingPawn);
+	if (!TargetAbilitySystemComponent)
+	{
+		return;
+	}
+
+	FGameplayCueParameters Params;
+	Params.Instigator = OtherActor;
+	TargetAbilitySystemComponent->ExecuteGameplayCue(EQGameplayTag::GameplayCue_InteractPrompt_Hide, Params);
+}
+
+void AEQItemBox::ApplyEffectToTarget(AActor* TargetActor)
+{
+	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (!TargetAbilitySystemComponent)
+	{
+		return;
+	}
+
+	FGameplayEffectContextHandle EffectContext = TargetAbilitySystemComponent->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetAbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, 1,
+		EffectContext);
+	if (EffectSpecHandle.IsValid())
+	{
+		TargetAbilitySystemComponent->BP_ApplyGameplayEffectSpecToSelf(EffectSpecHandle);
+	}
+}
+
+void AEQItemBox::InvokeGameplayCue(AActor* TargetActor)
+{
+	FGameplayCueParameters Parameters;
+	Parameters.SourceObject = this;
+	Parameters.Instigator = TargetActor;
+	Parameters.Location = GetActorLocation();
+
+	AbilitySystemComponent->ExecuteGameplayCue(EQGameplayTag::GameplayCue_ItemBox_Open, Parameters);
+}
---
+++ Source/EternalQuest/Item/EQItemBox.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/Actor.h"
+#include "Interface/EQCharacterInteractable.h"
+#include "EQItemBox.generated.h"
+
+class UWidgetComponent;
+class UEQInteractPromptWidget;
+class UGameplayEffect;
+class UEQItemData;
+class UBoxComponent;
+
+UCLASS()
+class ETERNALQUEST_API AEQItemBox : public AActor, public IAbilitySystemInterface, public IEQCharacterInteractable
+{
+	GENERATED_BODY()
+
+public:
+	AEQItemBox();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	virtual void PerformInteract(AActor* InteractActor) override;
+
+protected:
+	virtual void PostInitializeComponents() override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
+
+	void ApplyEffectToTarget(AActor* TargetActor);
+	void InvokeGameplayCue(AActor* TargetActor);
+
+private:
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = Box, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UBoxComponent> TriggerBoxComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = Box, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+
+	// UPROPERTY(EditAnywhere, Category = Item, Meta = (AllowPrivateAccess = true))
+	// TObjectPtr<UEQItemData> ItemData;
+
+	UPROPERTY(EditAnywhere, Category = Item, Meta = (AllowPrivateAccess = true))
+	TArray<UEQItemData*> ItemDataArray;
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<UGameplayEffect> GameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	FGameplayTag GameplayCueTag;
+};
