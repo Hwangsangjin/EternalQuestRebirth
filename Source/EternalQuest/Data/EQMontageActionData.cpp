a9df485ce312615c6923e38124d6f109b8813d16
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-02 17:10:34 +0900

    몽타주 액션 데이터 클래스 추가

---
+++ Source/EternalQuest/Data/EQMontageActionData.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQMontageActionData.h"
+
+UEQMontageActionData::UEQMontageActionData()
+{
+}
+
+UAnimMontage* UEQMontageActionData::GetMontageForTag(const FGameplayTag& GameplayTag, const int32 Index) const
+{
+	const FEQMontageGroup* MontageGroup = MontageGroupMap.Find(GameplayTag);
+	if (!MontageGroup)
+	{
+		return nullptr;
+	}
+
+	if (MontageGroup->AnimMontages.IsValidIndex(Index))
+	{
+		return MontageGroup->AnimMontages[Index];
+	}
+
+	return nullptr;
+}
+
+UAnimMontage* UEQMontageActionData::GetRandomMontageForTag(const FGameplayTag& GameplayTag) const
+{
+	const FEQMontageGroup* MontageGroup = MontageGroupMap.Find(GameplayTag);
+	if (!MontageGroup)
+	{
+		return nullptr;
+	}
+
+	const int32 RandomIndex = FMath::RandRange(0, MontageGroup->AnimMontages.Num() - 1);
+
+	if (MontageGroup->AnimMontages.IsValidIndex(RandomIndex))
+	{
+		return GetMontageForTag(GameplayTag, RandomIndex);
+	}
+
+	return nullptr;
+}
---
+++ Source/EternalQuest/Data/EQMontageActionData.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "EQMontageActionData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEQMontageGroup
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Montage)
+	TArray<UAnimMontage*> AnimMontages;
+};
+
+UCLASS()
+class ETERNALQUEST_API UEQMontageActionData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UEQMontageActionData();
+
+	UAnimMontage* GetMontageForTag(const FGameplayTag& GameplayTag, const int32 Index) const;
+	UAnimMontage* GetRandomMontageForTag(const FGameplayTag& GameplayTag) const;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Montage, Meta = (AllowPrivateAccess = true))
+	TMap<FGameplayTag, FEQMontageGroup> MontageGroupMap;
+};
