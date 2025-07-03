7d8f48cd6650a416f05c29899ca6a9ae3fef0222
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-03 15:29:01 +0900

    방패 클래스 추가

---
+++ Source/EternalQuest/Equipment/EQShield.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQShield.h"
+#include "EQWeapon.h"
+#include "Animation/EQAnimInstance.h"
+#include "Component/EQCombatComponent.h"
+
+class UEQAnimInstance;
+
+AEQShield::AEQShield()
+{
+}
+
+void AEQShield::EquipItem()
+{
+	Super::EquipItem();
+
+	const AActor* OwnerActor = GetOwner();
+	if (!OwnerActor)
+	{
+		return;
+	}
+
+	UEQCombatComponent* CombatComponent = OwnerActor->GetComponentByClass<UEQCombatComponent>();
+	if (!CombatComponent)
+	{
+		return;
+	}
+
+	const AEQWeapon* Weapon = CombatComponent->GetWeapon();
+	if (!Weapon)
+	{
+		return;
+	}
+
+	CombatComponent->SetShield(this);
+
+	const FName SocketName = CombatComponent->IsCombatEnabled() ? EquipSocketName : UnequipSocketName;
+	AttachToOwner(SocketName);
+}
---
+++ Source/EternalQuest/Equipment/EQShield.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQEquipment.h"
+#include "EQShield.generated.h"
+
+class UEQCombatComponent;
+
+UCLASS()
+class ETERNALQUEST_API AEQShield : public AEQEquipment
+{
+	GENERATED_BODY()
+
+public:
+	AEQShield();
+
+	virtual void EquipItem() override;
+
+	FORCEINLINE FName GetEquipSocketName() const { return EquipSocketName; }
+	FORCEINLINE FName GetUnequipSocketName() const { return UnequipSocketName; }
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Socket, Meta=(AllowPrivateAccess = true))
+	FName EquipSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Socket, Meta=(AllowPrivateAccess = true))
+	FName UnequipSocketName;
+};
