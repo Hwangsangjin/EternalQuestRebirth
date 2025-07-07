bc344ba8cc4983a6efe2e2c630bb1f4bb8fea4bb
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-07 23:37:16 +0900

    베이스 캐릭터 클래스 추가

---
+++ Source/EternalQuest/Character/EQBaseCharacter.cpp
@@ -0,0 +1,383 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBaseCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Component/EQAttributeComponent.h"
+#include "Component/EQCombatComponent.h"
+#include "Component/EQInventoryComponent.h"
+#include "Component/EQRotationComponent.h"
+#include "Component/EQStateComponent.h"
+#include "Component/EQWidgetComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Data/EQCharacterControlData.h"
+#include "Engine/DamageEvents.h"
+#include "Equipment/EQShield.h"
+#include "Equipment/EQWeapon.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameplayTag/EQGameplayTag.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Physics/EQCollision.h"
+#include "Sound/SoundCue.h"
+
+AEQBaseCharacter::AEQBaseCharacter()
+{
+	bReplicates = true;
+	AbilitySystemComponent = nullptr;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCapsuleComponent()->InitCapsuleSize(34.0f, 88.0f);
+	GetCapsuleComponent()->SetCollisionProfileName(CPROFILE_EQCAPSULE);
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->JumpZVelocity = 500.0f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 450.0f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.0f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.0f;
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+	GetMesh()->SetAnimationMode(EAnimationMode::Type::AnimationBlueprint);
+	GetMesh()->SetCollisionProfileName(TEXT("NoCollision"));
+
+	AttributeComponent = CreateDefaultSubobject<UEQAttributeComponent>(TEXT("AttributeComponent"));
+	AttributeComponent->OnDeadDelegate.AddUObject(this, &ThisClass::SetDead);
+
+	StateComponent = CreateDefaultSubobject<UEQStateComponent>(TEXT("StateComponent"));
+
+	CombatComponent = CreateDefaultSubobject<UEQCombatComponent>(TEXT("CombatComponent"));
+
+	RotationComponent = CreateDefaultSubobject<UEQRotationComponent>(TEXT("RotationComponent"));
+
+	InventoryComponent = CreateDefaultSubobject<UEQInventoryComponent>(TEXT("InventoryComponent"));
+
+	HealthBarWidgetComponent = CreateDefaultSubobject<UEQWidgetComponent>(TEXT("HealthBarWidgetComponent"));
+	HealthBarWidgetComponent->SetupAttachment(RootComponent);
+	HealthBarWidgetComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 90.0f));
+	HealthBarWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	HealthBarWidgetComponent->SetDrawSize(FVector2D(200.0f, 20.0f));
+}
+
+bool AEQBaseCharacter::IsDead() const
+{
+	check(StateComponent);
+
+	FGameplayTagContainer TagContainer;
+	TagContainer.AddTag(EQGameplayTag::Character_State_Dead);
+
+	return StateComponent->IsCurrentStateEqualToAny(TagContainer);
+}
+
+void AEQBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CombatComponent->SetCombatEnabled(true);
+	AbilitySystemComponent->AddLooseGameplayTag(EQGameplayTag::Character_State_Combat);
+
+	FActorSpawnParameters Parameters;
+	Parameters.Owner = this;
+
+	if (WeaponClass)
+	{
+		AEQWeapon* Weapon = GetWorld()->SpawnActor<AEQWeapon>(WeaponClass, GetActorTransform(), Parameters);
+		Weapon->EquipItem();
+	}
+
+	if (ShieldClass)
+	{
+		AEQShield* Shield = GetWorld()->SpawnActor<AEQShield>(ShieldClass, GetActorTransform(), Parameters);
+		Shield->EquipItem();
+	}
+}
+
+UAbilitySystemComponent* AEQBaseCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AEQBaseCharacter::SetCharacterControlData(const UEQCharacterControlData* CharacterControlData)
+{
+	bUseControllerRotationYaw = CharacterControlData->bUseControllerRotationYaw;
+
+	GetCharacterMovement()->bOrientRotationToMovement = CharacterControlData->bOrientRotationToMovement;
+	GetCharacterMovement()->bUseControllerDesiredRotation = CharacterControlData->bUseControllerDesiredRotation;
+	GetCharacterMovement()->RotationRate = CharacterControlData->RotationRate;
+}
+
+float AEQBaseCharacter::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
+{
+	const float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	check(AttributeComponent);
+	check(StateComponent);
+
+	bFacing = UKismetMathLibrary::InRange_FloatFloat(GetDotProductTo(EventInstigator->GetPawn()), -0.1f, 1.0f);
+
+	if (CanPerformBlock(EventInstigator))
+	{
+		AttributeComponent->TakeDamageAmount(0.0f);
+		StateComponent->SetState(EQGameplayTag::Character_State_Blocking);
+	}
+	else
+	{
+		AttributeComponent->TakeDamageAmount(ActualDamage);
+		StateComponent->SetState(EQGameplayTag::Character_State_Hit);
+	}
+
+	StateComponent->ToggleMovementInput(false);
+
+	if (DamageEvent.IsOfType(FPointDamageEvent::ClassID))
+	{
+		const FPointDamageEvent* PointDamageEvent = static_cast<const FPointDamageEvent*>(&DamageEvent);
+
+		const FVector ShotDirection = PointDamageEvent->ShotDirection;
+		const FVector ImpactPoint = PointDamageEvent->HitInfo.ImpactPoint;
+		const FVector ImpactDirection = PointDamageEvent->HitInfo.ImpactNormal;
+		const FVector HitLocation = PointDamageEvent->HitInfo.Location;
+
+		ImpactEffect(EventInstigator, ImpactPoint);
+
+		HitReaction(EventInstigator->GetPawn());
+	}
+
+	return ActualDamage;
+}
+
+bool AEQBaseCharacter::IsCombatEnabled() const
+{
+	check(CombatComponent);
+	return CombatComponent->IsCombatEnabled();
+}
+
+void AEQBaseCharacter::SetCombatEnabled(bool bEnabled)
+{
+	check(CombatComponent);
+	return CombatComponent->SetCombatEnabled(bEnabled);
+}
+
+AEQWeapon* AEQBaseCharacter::GetWeapon() const
+{
+	check(CombatComponent);
+	return CombatComponent->GetWeapon();
+}
+
+AEQShield* AEQBaseCharacter::GetShield() const
+{
+	check(CombatComponent);
+	return CombatComponent->GetShield();
+}
+
+void AEQBaseCharacter::ActivateWeaponCollision()
+{
+	check(CombatComponent);
+	CombatComponent->GetWeapon()->ActivateCollision();
+}
+
+void AEQBaseCharacter::DeactivateWeaponCollision()
+{
+	check(CombatComponent);
+	CombatComponent->GetWeapon()->DeactivateCollision();
+}
+
+bool AEQBaseCharacter::CanBlockStance() const
+{
+	check(StateComponent);
+	check(CombatComponent);
+
+	const AEQWeapon* Weapon = CombatComponent->GetWeapon();
+	if (!Weapon)
+	{
+		return false;
+	}
+
+	FGameplayTagContainer TagContainer;
+	TagContainer.AddTag(EQGameplayTag::Character_State_Attacking);
+	TagContainer.AddTag(EQGameplayTag::Character_State_Hit);
+	TagContainer.AddTag(EQGameplayTag::Character_State_Jumping);
+
+	return StateComponent->IsCurrentStateEqualToAny(TagContainer) == false && Weapon->GetCombatType() == ECombatType::SwordShield;
+}
+
+bool AEQBaseCharacter::CanPerformBlock(const AActor* Attacker) const
+{
+	if (!Attacker)
+	{
+		return false;
+	}
+
+	check(CombatComponent);
+	check(AttributeComponent);
+	check(StateComponent);
+
+	if (!CombatComponent->IsBlockingEnabled())
+	{
+		return false;
+	}
+
+	FGameplayTagContainer InvalidStates;
+	InvalidStates.AddTag(EQGameplayTag::Character_State_Attacking);
+	InvalidStates.AddTag(EQGameplayTag::Character_State_Hit);
+	InvalidStates.AddTag(EQGameplayTag::Character_State_Jumping);
+
+	if (StateComponent->IsCurrentStateEqualToAny(InvalidStates))
+	{
+		return false;
+	}
+
+	const float Dot = GetDotProductTo(Attacker);
+	const bool bIsFacing = UKismetMathLibrary::InRange_FloatFloat(Dot, -0.1f, 1.0f);
+
+	if (!bIsFacing)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool AEQBaseCharacter::CanPerformParry() const
+{
+	check(StateComponent);
+	check(CombatComponent);
+	check(AttributeComponent);
+
+	const AEQWeapon* Weapon = CombatComponent->GetWeapon();
+	if (!Weapon)
+	{
+		return false;
+	}
+
+	FGameplayTagContainer GameplayTags;
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Attacking);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Rolling);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Hit);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Blocking);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Parrying);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Dead);
+
+	return StateComponent->IsCurrentStateEqualToAny(GameplayTags) == false && Weapon->GetCombatType() == ECombatType::SwordShield;
+}
+
+bool AEQBaseCharacter::ParriedAttackSucceed() const
+{
+	check(StateComponent);
+
+	FGameplayTagContainer GameplayTags;
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Parrying);
+
+	return StateComponent->IsCurrentStateEqualToAny(GameplayTags) && bFacing;
+}
+
+bool AEQBaseCharacter::CanDrinkPotion() const
+{
+	check(InventoryComponent);
+	check(StateComponent);
+
+	FGameplayTagContainer GameplayTags;
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Attacking);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Jumping);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Hit);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Blocking);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Parrying);
+	GameplayTags.AddTag(EQGameplayTag::Character_State_Dead);
+
+	return InventoryComponent->GetPotionQuantity() > 0 && StateComponent->IsCurrentStateEqualToAny(GameplayTags) == false;
+}
+
+void AEQBaseCharacter::InterruptWhileDrinkingPotion() const
+{
+	check(StateComponent);
+
+	FGameplayTagContainer GameplayTags;
+	GameplayTags.AddTag(EQGameplayTag::Character_State_DrinkingPotion);
+
+	if (StateComponent->IsCurrentStateEqualToAny(GameplayTags))
+	{
+		if (InventoryComponent)
+		{
+			InventoryComponent->DespawnPotion();
+		}
+	}
+}
+
+void AEQBaseCharacter::ImpactEffect(const AActor* Attacker, const FVector& Location)
+{
+	if (CanPerformBlock(Attacker))
+	{
+		ShieldBlockingEffect(Location);
+	}
+	else
+	{
+		if (!ImpactSoundCue || !ImpactNiagaraSystem)
+		{
+			return;
+		}
+
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), ImpactSoundCue, Location);
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ImpactNiagaraSystem, GetActorLocation(), GetActorRotation());
+	}
+}
+
+void AEQBaseCharacter::ShieldBlockingEffect(const FVector& Location)
+{
+	if (!BlockingSoundCue || !BlockingNiagaraSystem)
+	{
+		return;
+	}
+
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), BlockingSoundCue, Location);
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), BlockingNiagaraSystem, GetActorLocation(), GetActorRotation());
+}
+
+void AEQBaseCharacter::HitReaction(const AActor* Attacker)
+{
+	check(CombatComponent);
+
+	if (CanPerformBlock(Attacker))
+	{
+		if (UAnimMontage* BlockingHitMontage = CombatComponent->GetWeapon()->GetMontageForTag(EQGameplayTag::Character_Action_BlockingHit))
+		{
+			PlayAnimMontage(BlockingHitMontage);
+		}
+	}
+	else
+	{
+		if (UAnimMontage* HitReactionMontage = CombatComponent->GetWeapon()->GetHitReactionMontage(Attacker))
+		{
+			PlayAnimMontage(HitReactionMontage);
+		}
+	}
+}
+
+void AEQBaseCharacter::SetDead()
+{
+	StateComponent->SetState(EQGameplayTag::Character_State_Dead);
+	GetCharacterMovement()->SetMovementMode(MOVE_None);
+	SetActorEnableCollision(false);
+	HideHealthBarWidget();
+	PlayDeadAnimation();
+}
+
+void AEQBaseCharacter::HideHealthBarWidget()
+{
+	check(HealthBarWidgetComponent);
+	HealthBarWidgetComponent->SetHiddenInGame(true);
+}
+
+void AEQBaseCharacter::PlayDeadAnimation()
+{
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (!AnimInstance)
+	{
+		return;
+	}
+
+	AnimInstance->StopAllMontages(0.0f);
+	AnimInstance->Montage_Play(DeadMontage);
+}
---
+++ Source/EternalQuest/Character/EQBaseCharacter.h
@@ -0,0 +1,177 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "Interface/EQCombatCapable.h"
+#include "EQBaseCharacter.generated.h"
+
+class UEQInventoryComponent;
+class UNiagaraSystem;
+class USoundCue;
+class UEQRotationComponent;
+class UEQCombatComponent;
+class UEQStateComponent;
+class UEQAttributeComponent;
+class UGameplayAbility;
+class UEQCharacterAttributeSet;
+class UEQWidgetComponent;
+class UInputAction;
+class UEQCharacterControlData;
+class UEQComboActionData;
+
+UENUM(BlueprintType)
+enum class ECharacterControlType : uint8
+{
+	Default	UMETA(DisplayName = "Default")
+};
+
+UENUM(BlueprintType)
+enum class EHitDirection : uint8
+{
+	Front	UMETA(DisplayName = "Front"),
+	Back	UMETA(DisplayName = "Back"),
+	Left	UMETA(DisplayName = "Left"),
+	Right	UMETA(DisplayName = "Right")
+};
+
+UENUM(BlueprintType)
+enum class ECombatType : uint8
+{
+	None		UMETA(DisplayName = "None"),
+	SwordShield	UMETA(DisplayName = "SwordShield"),
+	TwoHanded	UMETA(DisplayName = "TwoHanded"),
+};
+
+UCLASS()
+class ETERNALQUEST_API AEQBaseCharacter : public ACharacter, public IAbilitySystemInterface, public IEQCombatCapable
+{
+	GENERATED_BODY()
+
+public:
+	AEQBaseCharacter();
+
+	FORCEINLINE UEQStateComponent* GetStateComponent() const { return StateComponent; }
+
+	FORCEINLINE UAnimMontage* GetAttackMontage() const { return AttackMontage; }
+	FORCEINLINE UEQComboActionData* GetComboActionData() const { return ComboActionData; }
+	FORCEINLINE virtual UAnimMontage* GetSkillMontage() const { return SkillMontage; }
+	FORCEINLINE virtual UAnimMontage* GetRollMontage() const { return RollMontage; }
+
+	void HitReaction(const AActor* Attacker);
+
+	bool CanPerformBlock(const AActor* Attacker) const;
+
+	bool CanPerformParry() const;
+	bool ParriedAttackSucceed() const;
+
+	bool CanDrinkPotion() const;
+	void InterruptWhileDrinkingPotion() const;
+
+	bool IsDead() const;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	virtual void SetCharacterControlData(const UEQCharacterControlData* CharacterControlData);
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	virtual AEQWeapon* GetWeapon() const override;
+	virtual AEQShield* GetShield() const override;
+	virtual bool IsCombatEnabled() const override;
+	virtual void SetCombatEnabled(bool bEnabled) override;
+	virtual void ActivateWeaponCollision() override;
+	virtual void DeactivateWeaponCollision() override;
+
+	bool CanBlockStance() const;
+
+	void ImpactEffect(const AActor* Attacker, const FVector& Location);
+	void ShieldBlockingEffect(const FVector& Location);
+
+	UFUNCTION()
+	virtual void SetDead();
+
+	void HideHealthBarWidget();
+	void PlayDeadAnimation();
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TArray<TSubclassOf<UGameplayAbility>> StartAbilities;
+
+	UPROPERTY(VisibleAnywhere, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQAttributeComponent> AttributeComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQStateComponent> StateComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQCombatComponent> CombatComponent;
+
+	UPROPERTY(EditAnywhere, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQRotationComponent> RotationComponent;
+
+	UPROPERTY(EditAnywhere, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQInventoryComponent> InventoryComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = Component, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQWidgetComponent> HealthBarWidgetComponent;
+
+	UPROPERTY(EditAnywhere, Category = CharacterControl, Meta = (AllowPrivateAccess = true))
+	TMap<ECharacterControlType, UEQCharacterControlData*> CharacterControlMap;
+
+	UPROPERTY(EditAnywhere, Category = Montage, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAnimMontage> DeadMontage;
+
+	UPROPERTY(EditAnywhere, Category = Montage, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAnimMontage> AttackMontage;
+
+	UPROPERTY(EditAnywhere, Category = Montage, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAnimMontage> SkillMontage;
+
+	UPROPERTY(EditAnywhere, Category = Montage, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAnimMontage> RollMontage;
+
+	UPROPERTY(EditAnywhere, Category = Montage, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAnimMontage> DrinkMontage;
+
+	UPROPERTY(EditAnywhere, Category = Combo, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UEQComboActionData> ComboActionData;
+
+	UPROPERTY(EditAnywhere, Category = Combo, Meta = (AllowPrivateAccess = true))
+	int32 CurrentComboIndex;
+
+	UPROPERTY(EditAnywhere, Category = Combo, Meta = (AllowPrivateAccess = true))
+	FTimerHandle ComboTimerHandle;
+
+	UPROPERTY(EditAnywhere, Category = Combo, Meta = (AllowPrivateAccess = true))
+	uint8 bHasNextComboCommand : 1;
+
+	UPROPERTY(EditAnywhere, Category = Weapon, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<AEQWeapon> WeaponClass;
+
+	UPROPERTY(EditAnywhere, Category = Weapon, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<AEQShield> ShieldClass;
+
+	UPROPERTY(EditAnywhere, Category = Effect, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<USoundCue> ImpactSoundCue;
+
+	UPROPERTY(EditAnywhere, Category = Effect, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<USoundCue> BlockingSoundCue;
+
+	UPROPERTY(EditAnywhere, Category = Effect, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UNiagaraSystem> ImpactNiagaraSystem;
+
+	UPROPERTY(EditAnywhere, Category = Effect, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UNiagaraSystem> BlockingNiagaraSystem;
+
+	UPROPERTY(EditAnywhere, Category = Value, Meta = (AllowPrivateAccess = true))
+	uint8 bSprinting : 1;
+
+	UPROPERTY(EditAnywhere, Category = Value, Meta = (AllowPrivateAccess = true))
+	uint8 bFacing : 1;
+};
