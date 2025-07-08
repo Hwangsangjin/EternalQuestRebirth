ff7e0bf82872511fe6bf4bfc70f3fb4490bbbf38
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-08 20:00:06 +0900

    캐릭터 어트리뷰트셋 클래스 추가

---
+++ Source/EternalQuest/Attribute/EQCharacterAttributeSet.cpp
@@ -0,0 +1,129 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQCharacterAttributeSet.h"
+#include "AbilitySystemGlobals.h"
+#include "GameplayEffectExtension.h"
+#include "GameplayAbility/EQGameplayAbility_Sprint.h"
+#include "GameplayTag/EQGameplayTag.h"
+
+UEQCharacterAttributeSet::UEQCharacterAttributeSet()
+	: Damage(0.0f)
+	, AttackRange(100.0f)
+	, MaxAttackRange(300.0f)
+	, AttackRadius(50.0f)
+	, MaxAttackRadius(150.0f)
+	, AttackRate(30.0f)
+	, MaxAttackRate(100.0f)
+	, MaxHealth(100.0f)
+	, MaxMana(100.0f)
+	, MaxStamina(100.0f)
+{
+	InitHealth(GetMaxHealth());
+	InitMana(GetMaxMana());
+	InitStamina(GetMaxStamina());
+}
+
+void UEQCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	// if (Attribute == GetHealthAttribute())
+	// {
+	// 	NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	// }
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxMana());
+	}
+
+	if (Attribute == GetStaminaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxStamina());
+	}
+
+	if (Attribute == GetDamageAttribute())
+	{
+		NewValue = NewValue < 0.0f ? 0.0f : NewValue;
+	}
+}
+
+bool UEQCharacterAttributeSet::PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data)
+{
+	if (!Super::PreGameplayEffectExecute(Data))
+	{
+		return false;
+	}
+
+	if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		if (Data.EvaluatedData.Magnitude > 0.0f)
+		{
+			if (Data.Target.HasMatchingGameplayTag(EQGameplayTag::Character_State_Invincible))
+			{
+				Data.EvaluatedData.Magnitude = 0.0f;
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+void UEQCharacterAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	constexpr float MinimumValue = 0.0f;
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), MinimumValue, GetMaxHealth()));
+	}
+	else if (Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		SetMana(FMath::Clamp(GetMana(), MinimumValue, GetMaxMana()));
+	}
+	else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
+	{
+		SetStamina(FMath::Clamp(GetStamina(), MinimumValue, GetMaxStamina()));
+
+		if (GetStamina() <= MinimumValue)
+		{
+			UAbilitySystemComponent* AbilitySystemComponent = Data.Target.AbilityActorInfo->AbilitySystemComponent.Get();
+			if (AbilitySystemComponent)
+			{
+				AbilitySystemComponent->RemoveLooseGameplayTag(EQGameplayTag::Character_State_Sprinting);
+
+				FGameplayTagContainer TagContainer;
+				TagContainer.AddTag(EQGameplayTag::Character_State_Sprinting);
+				AbilitySystemComponent->CancelAbilities(&TagContainer);
+
+				FGameplayAbilitySpec* Spec = AbilitySystemComponent->FindAbilitySpecFromClass(UEQGameplayAbility_Sprint::StaticClass());
+				if (Spec)
+				{
+					Spec->InputPressed = false;
+				}
+			}
+		}
+	}
+	else if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth() - GetDamage(), MinimumValue, GetMaxHealth()));
+		SetDamage(MinimumValue);
+	}
+
+	if (GetHealth() <= MinimumValue && !bOutOfHealth)
+	{
+		Data.Target.AddLooseGameplayTag(EQGameplayTag::Character_State_Dead);
+		OnOutOfHealth.Broadcast();
+	}
+
+	bOutOfHealth = GetHealth() <= MinimumValue;
+}
+
+// void UEQCharacterAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+// {
+// 	if (Attribute == GetHealthAttribute())
+// 	{
+// 		UE_LOG(LogTemp, Warning, TEXT("Health: %f -> %f"), OldValue, NewValue);
+// 	}
+// }
---
+++ Source/EternalQuest/Attribute/EQCharacterAttributeSet.h
@@ -0,0 +1,95 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "EQCharacterAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)	   \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)			   \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)			   \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOutOfHealthDelegate);
+
+class UEQGameplayEffect_AttackDamage;
+
+UCLASS()
+class ETERNALQUEST_API UEQCharacterAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UEQCharacterAttributeSet();
+
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, Damage)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, AttackRange)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, MaxAttackRange)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, AttackRadius)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, MaxAttackRadius)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, AttackRate)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, MaxAttackRate)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, Health)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, MaxHealth)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, Mana)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, MaxMana)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, Stamina)
+	ATTRIBUTE_ACCESSORS(UEQCharacterAttributeSet, MaxStamina)
+
+	UPROPERTY()
+	mutable FOnOutOfHealthDelegate OnOutOfHealth;
+
+protected:
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	//virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Damage;
+
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRange;
+
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRange;
+
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRadius;
+
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRadius;
+
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRate;
+
+	UPROPERTY(BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRate;
+
+	UPROPERTY(BlueprintReadOnly, Category = Health, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, Category = Health, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, Category = Health, Meta = (AllowPrivateAccess = true))
+	uint8 bOutOfHealth : 1;
+
+	UPROPERTY(BlueprintReadOnly, Category = Mana, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, Category = Mana, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxMana;
+
+	UPROPERTY(BlueprintReadOnly, Category = Stamina, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Stamina;
+
+	UPROPERTY(BlueprintReadOnly, Category = Stamina, Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxStamina;
+
+	friend UEQGameplayEffect_AttackDamage;
+};
