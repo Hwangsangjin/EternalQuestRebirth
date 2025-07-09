0232d58c21b6a981e75554c5bd3861254231c1d6
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-09 15:20:33 +0900

    충돌 컴포넌트 클래스 추가

---
+++ Source/EternalQuest/Component/EQCollisionComponent.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQCollisionComponent.h"
+
+UEQCollisionComponent::UEQCollisionComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	TraceRadius = 20.0f;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+	DrawDebugType = EDrawDebugTrace::Type::ForDuration;
+}
+
+void UEQCollisionComponent::SetPrimitiveComponent(class UPrimitiveComponent* NewPrimitiveComponent)
+{
+	PrimitiveComponent = NewPrimitiveComponent;
+}
+
+void UEQCollisionComponent::TurnOnCollision()
+{
+	AlreadyHitActors.Empty();
+	bIsCollisionEnabled = true;
+}
+
+void UEQCollisionComponent::TurnOffCollision()
+{
+	bIsCollisionEnabled = false;
+}
+
+void UEQCollisionComponent::AddIgnoredActor(AActor* Actor)
+{
+	IgnoreActors.Add(Actor);
+}
+
+void UEQCollisionComponent::RemoveIgnoredActor(AActor* Actor)
+{
+	IgnoreActors.Remove(Actor);
+}
+
+bool UEQCollisionComponent::CanHitActor(AActor* Actor) const
+{
+	return AlreadyHitActors.Contains(Actor) == false;
+}
+
+void UEQCollisionComponent::CollisionTrace()
+{
+	TArray<FHitResult> HitResults;
+
+	const FVector Start = PrimitiveComponent->GetSocketLocation(StartSocketName);
+	const FVector End = PrimitiveComponent->GetSocketLocation(EndSocketName);
+
+	bool const bHit = UKismetSystemLibrary::SphereTraceMultiForObjects(GetOwner(), Start, End, TraceRadius, ObjectTypes, false, IgnoreActors, DrawDebugType, HitResults, true);
+	if (!bHit)
+	{
+		return;
+	}
+
+	for (const FHitResult& HitResult : HitResults)
+	{
+		AActor* HitActor = HitResult.GetActor();
+		if (!HitActor || !CanHitActor(HitActor))
+		{
+			continue;
+		}
+
+		AlreadyHitActors.Add(HitActor);
+
+		if (OnHitDelegate.IsBound())
+		{
+			OnHitDelegate.Broadcast(HitResult);
+		}
+	}
+}
+
+void UEQCollisionComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (bIsCollisionEnabled)
+	{
+		CollisionTrace();
+	}
+}
---
+++ Source/EternalQuest/Component/EQCollisionComponent.h
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "EQCollisionComponent.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnHitDelegate, const FHitResult&)
+
+UCLASS(ClassGroup = (Custom), Meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQCollisionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQCollisionComponent();
+
+	void SetPrimitiveComponent(class UPrimitiveComponent* NewPrimitiveComponent);
+
+	void TurnOnCollision();
+	void TurnOffCollision();
+
+	void AddIgnoredActor(AActor* Actor);
+	void RemoveIgnoredActor(AActor* Actor);
+
+	bool CanHitActor(AActor* Actor) const;
+
+	void CollisionTrace();
+
+	FOnHitDelegate OnHitDelegate;
+
+protected:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	FName StartSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	FName EndSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	float TraceRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	TArray<AActor*> IgnoreActors;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	TEnumAsByte<EDrawDebugTrace::Type> DrawDebugType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UPrimitiveComponent> PrimitiveComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	TArray<AActor*> AlreadyHitActors;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collision, Meta = (AllowPrivateAccess = true))
+	uint8 bIsCollisionEnabled;
+};
