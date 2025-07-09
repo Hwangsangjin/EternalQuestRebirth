eb017a1b29b2dae9057a95c38f691bb8c7ce0122
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-09 15:21:36 +0900

    인벤토리 컴포넌트 클래스 추가

---
+++ Source/EternalQuest/Component/EQInventoryComponent.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQInventoryComponent.h"
+#include "EQAttributeComponent.h"
+#include "GameFramework/Character.h"
+#include "Item/EQPotion.h"
+
+UEQInventoryComponent::UEQInventoryComponent()
+{
+	PotionQuantity = 10;
+	PotionHealAmount = 15.0f;
+}
+
+void UEQInventoryComponent::DrinkPotion()
+{
+	if (PotionQuantity <= 0)
+	{
+		return;
+	}
+
+	UEQAttributeComponent* AttributeComponent = GetOwner()->GetComponentByClass<UEQAttributeComponent>();
+	if (!AttributeComponent)
+	{
+		return;
+	}
+
+	--PotionQuantity;
+	AttributeComponent->Heal(PotionHealAmount);
+}
+
+void UEQInventoryComponent::SpawnPotion()
+{
+	Potion = GetWorld()->SpawnActor<AEQPotion>(PotionClass, GetOwner()->GetActorTransform());
+	if (!Potion)
+	{
+		return;
+	}
+
+	const ACharacter* Character = Cast<ACharacter>(GetOwner());
+	if (!Character)
+	{
+		return;
+	}
+
+	Potion->AttachToComponent(Character->GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), PotionSocketName);
+}
+
+void UEQInventoryComponent::DespawnPotion()
+{
+	if (!Potion)
+	{
+		return;
+	}
+
+	Potion->Destroy();
+}
---
+++ Source/EternalQuest/Component/EQInventoryComponent.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EQInventoryComponent.generated.h"
+
+class AEQPotion;
+
+UCLASS(ClassGroup = (Custom), Meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQInventoryComponent();
+
+	void DrinkPotion();
+	void SpawnPotion();
+	void DespawnPotion();
+
+	FORCEINLINE int GetPotionQuantity() const { return PotionQuantity; }
+	FORCEINLINE void SetPointQuantity(int32 InQuantity) { PotionQuantity = InQuantity; };
+
+protected:
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Inventory, Meta = (AllowPrivateAccess = true))
+	int32 PotionQuantity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Inventory, Meta = (AllowPrivateAccess = true))
+	float PotionHealAmount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Inventory, Meta = (AllowPrivateAccess = true))
+	FName PotionSocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Inventory, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<AEQPotion> PotionClass;
+
+	UPROPERTY()
+	TObjectPtr<AEQPotion> Potion;
+};
