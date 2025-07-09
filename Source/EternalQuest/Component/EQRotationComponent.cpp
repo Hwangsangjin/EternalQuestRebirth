e87b4ce463a3cb08a236eb5e1d1d86b767ed393d
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-09 15:20:46 +0900

    회전 컴포넌트 클래스 추가

---
+++ Source/EternalQuest/Component/EQRotationComponent.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQRotationComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UEQRotationComponent::UEQRotationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UEQRotationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!TargetActor)
+	{
+		return;
+	}
+
+	if (!bShouldRotate)
+	{
+		return;
+	}
+
+	const FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetOwner()->GetActorLocation(), TargetActor->GetActorLocation());
+	GetOwner()->SetActorRotation(FRotator(0.0f, LookAtRotation.Yaw, 0.0f));
+}
---
+++ Source/EternalQuest/Component/EQRotationComponent.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EQRotationComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), Meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQRotationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQRotationComponent();
+
+	FORCEINLINE void SetTargetActor(AActor* InActor) { TargetActor = InActor; }
+	FORCEINLINE void ToggleShouldRotate(const bool bRotate) { bShouldRotate = bRotate; }
+
+protected:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Target, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<AActor> TargetActor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Target, Meta = (AllowPrivateAccess = true))
+	uint8 bShouldRotate : 1;
+};
