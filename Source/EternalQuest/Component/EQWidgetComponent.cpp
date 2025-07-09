04778ba83e028eceff220cd02008b7d519d020c5
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-09 15:19:09 +0900

    위젯 컴포넌트 클래스 추가

---
+++ Source/EternalQuest/Component/EQWidgetComponent.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQWidgetComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "UI/EQUserWidget.h"
+
+UEQWidgetComponent::UEQWidgetComponent()
+{
+}
+
+void UEQWidgetComponent::InitWidget()
+{
+	Super::InitWidget();
+
+	UAbilitySystemComponent* NewAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetOwner());
+	if (!NewAbilitySystemComponent)
+	{
+		return;
+	}
+
+	UEQUserWidget* EQUserWidget = Cast<UEQUserWidget>(GetWidget());
+	if (!EQUserWidget)
+	{
+		return;
+	}
+
+	EQUserWidget->SetAbilitySystemComponent(NewAbilitySystemComponent);
+}
---
+++ Source/EternalQuest/Component/EQWidgetComponent.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "EQWidgetComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), Meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQWidgetComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQWidgetComponent();
+
+protected:
+	virtual void InitWidget() override;
+};
