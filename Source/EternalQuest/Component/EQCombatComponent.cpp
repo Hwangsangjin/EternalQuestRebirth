3e3cf67b50f98a35e59eee1585fb98865255bd29
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-09 15:19:32 +0900

    전투 컴포넌트 클래스 추가

---
+++ Source/EternalQuest/Component/EQCombatComponent.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQCombatComponent.h"
+#include "Character/EQPlayerCharacter.h"
+#include "Equipment/EQShield.h"
+#include "Equipment/EQWeapon.h"
+#include "Item/EQPickupItem.h"
+
+UEQCombatComponent::UEQCombatComponent()
+{
+}
+
+void UEQCombatComponent::SetWeapon(AEQWeapon* NewWeapon)
+{
+	if (Weapon)
+	{
+		SpawnPickupItem(Weapon->GetClass());
+		Weapon->Destroy();
+	}
+
+	Weapon = NewWeapon;
+}
+
+void UEQCombatComponent::SetShield(AEQShield* NewShield)
+{
+	if (Shield)
+	{
+		SpawnPickupItem(Shield->GetClass());
+		Shield->Destroy();
+	}
+
+	Shield = NewShield;
+}
+
+void UEQCombatComponent::SetCombatEnabled(const bool bEnabled)
+{
+	bCombatEnabled = bEnabled;
+
+	if (OnChangedCombatDelegate.IsBound())
+	{
+		OnChangedCombatDelegate.Broadcast(bCombatEnabled);
+	}
+}
+
+void UEQCombatComponent::SetBlockingEnabled(const bool bEnabled)
+{
+	bBlockingEnabled = bEnabled;
+
+	if (OnChangedCombatDelegate.IsBound())
+	{
+		OnChangedCombatDelegate.Broadcast(bBlockingEnabled);
+	}
+}
+
+void UEQCombatComponent::SpawnPickupItem(const TSubclassOf<AEQEquipment>& NewEquipmentClass) const
+{
+	const AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	const FVector SpawnLocation = Owner->GetActorLocation() - Owner->GetActorForwardVector() * 100.0f;
+	const FTransform SpawnTransform = FTransform(SpawnLocation);
+
+	AEQPickupItem* PickupItem = GetWorld()->SpawnActorDeferred<AEQPickupItem>(AEQPickupItem::StaticClass(), SpawnTransform, nullptr, nullptr, ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn);
+	if (!PickupItem)
+	{
+		return;
+	}
+
+	PickupItem->SetEquipmentClass(NewEquipmentClass);
+	PickupItem->FinishSpawning(SpawnTransform);
+	PickupItem->Throw();
+}
---
+++ Source/EternalQuest/Component/EQCombatComponent.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EQCombatComponent.generated.h"
+
+class AEQEquipment;
+class AEQShield;
+class AEQWeapon;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnChangedCombatDelegate, bool);
+
+UCLASS(ClassGroup = (Custom), Meta = (BlueprintSpawnableComponent))
+class ETERNALQUEST_API UEQCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEQCombatComponent();
+
+	void SetWeapon(AEQWeapon* NewWeapon);
+	void SetShield(AEQShield* NewShield);
+
+	void SetCombatEnabled(const bool bEnabled);
+	void SetBlockingEnabled(const bool bEnabled);
+
+	FORCEINLINE bool IsCombatEnabled() const { return bCombatEnabled; }
+	FORCEINLINE bool IsBlockingEnabled() const { return bBlockingEnabled; }
+
+	FORCEINLINE AEQWeapon* GetWeapon() const { return Weapon; }
+	FORCEINLINE AEQShield* GetShield() const { return Shield; }
+
+	FOnChangedCombatDelegate OnChangedCombatDelegate;
+
+private:
+	void SpawnPickupItem(const TSubclassOf<AEQEquipment>& NewEquipmentClass) const;
+
+	UPROPERTY()
+	TObjectPtr<AEQWeapon> Weapon;
+
+	UPROPERTY()
+	TObjectPtr<AEQShield> Shield;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, Meta = (AllowPrivateAccess = true))
+	uint8 bCombatEnabled : 1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, Meta = (AllowPrivateAccess = true))
+	uint8 bBlockingEnabled : 1;
+};
