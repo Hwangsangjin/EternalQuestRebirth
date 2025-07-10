b99fa156bc5813d32b4bd626a790cc8bf1341c4b
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-10 20:16:18 +0900

    물리 충돌 전처리기 추가

---
+++ Source/EternalQuest/Physics/EQCollision.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define CPROFILE_EQCAPSULE TEXT("EQCapsule")
+#define CPROFILE_EQTRIGGER TEXT("EQTrigger")
+#define CCHANNEL_EQPICKUP ECC_GameTraceChannel1
+#define CCHANNEL_EQTARGET ECC_GameTraceChannel2
+#define CCHANNEL_EQACTION ECC_GameTraceChannel3
