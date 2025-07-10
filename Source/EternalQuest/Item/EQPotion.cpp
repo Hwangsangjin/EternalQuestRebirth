ef77dc818b6ba3c0587f5dabafe5af28b7fcc190
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-10 20:17:24 +0900

    포션 클래스 추가

---
+++ Source/EternalQuest/Item/EQPotion.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQPotion.h"
+
+AEQPotion::AEQPotion()
+{
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	StaticMeshComponent->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+	SetRootComponent(StaticMeshComponent);
+}
---
+++ Source/EternalQuest/Item/EQPotion.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EQPotion.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQPotion : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEQPotion();
+
+protected:
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mesh, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+};
