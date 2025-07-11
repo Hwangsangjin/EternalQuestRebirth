6bbd013aebc18b8553c0ca616d14a3b2a2b62d47
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-11 19:46:23 +0900

    전투 능력 인터페이스 추가

---
+++ Source/EternalQuest/Interface/EQCombatCapable.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EQCombatCapable.generated.h"
+
+struct FGameplayTag;
+class AEQShield;
+class AEQWeapon;
+
+UINTERFACE()
+class UEQCombatCapable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ETERNALQUEST_API IEQCombatCapable
+{
+	GENERATED_BODY()
+
+public:
+	virtual AEQWeapon* GetWeapon() const = 0;
+	virtual AEQShield* GetShield() const = 0;
+	virtual bool IsCombatEnabled() const = 0;
+	virtual void SetCombatEnabled(bool bEnabled) = 0;
+	virtual void ActivateWeaponCollision() = 0;
+	virtual void DeactivateWeaponCollision() = 0;
+
+	virtual void PerformAttack(const FGameplayTag& AttackTypeTag, FOnMontageEnded& OnMontageEnded) {}
+
+	virtual void Parried() {}
+};
