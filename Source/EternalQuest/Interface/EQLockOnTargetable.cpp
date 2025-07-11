15b2cb4c57366a011a6ee5f65fd96e80cdf45ee5
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-11 19:47:19 +0900

    락온 타겟 인터페이스 추가

---
+++ Source/EternalQuest/Interface/EQLockOnTargetable.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQLockOnTargetable.h"
+
+// Add default functionality here for any IEQLockOnTargetable functions that are not pure virtual.
---
+++ Source/EternalQuest/Interface/EQLockOnTargetable.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EQLockOnTargetable.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnTargetOutOfHealthDelegate);
+
+UINTERFACE()
+class UEQLockOnTargetable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ETERNALQUEST_API IEQLockOnTargetable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnTargeted(bool bTargeted) = 0;
+	virtual bool CanBeTargeted() = 0;
+	virtual FOnTargetOutOfHealthDelegate& OnTargetOutOfHealth() = 0;
+};
