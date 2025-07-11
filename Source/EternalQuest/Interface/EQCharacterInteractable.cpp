67590fd668441bd819003904aca0da8cabc56a37
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-11 19:43:56 +0900

    캐릭터 상호작용 인터페이스 추가

---
+++ Source/EternalQuest/Interface/EQCharacterInteractable.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQCharacterInteractable.h"
+
+// Add default functionality here for any IEQInteract functions that are not pure virtual.
---
+++ Source/EternalQuest/Interface/EQCharacterInteractable.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EQCharacterInteractable.generated.h"
+
+UINTERFACE()
+class UEQCharacterInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ETERNALQUEST_API IEQCharacterInteractable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void PerformInteract(AActor* InteractActor) = 0;
+};
