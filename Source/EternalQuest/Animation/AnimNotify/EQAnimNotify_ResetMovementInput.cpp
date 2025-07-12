6a052c24c03ba2f8b1dd444e523577aa0ecf8306
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-12 18:22:33 +0900

    이동 입력 초기화 노티파이 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_ResetMovementInput.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotify_ResetMovementInput.h"
+#include "Component/EQStateComponent.h"
+
+void UEQAnimNotify_ResetMovementInput::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	const AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	UEQStateComponent* StateComponent = Owner->GetComponentByClass<UEQStateComponent>();
+	if (!StateComponent)
+	{
+		return;
+	}
+
+	StateComponent->ToggleMovementInput(true);
+}
