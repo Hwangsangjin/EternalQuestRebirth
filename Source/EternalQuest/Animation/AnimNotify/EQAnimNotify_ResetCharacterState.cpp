86956bbfb72a88baf8d36e44fcdbf28d069db38f
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-12 18:22:15 +0900

    캐릭터 상태 초기화 노티파이 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_ResetCharacterState.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotify_ResetCharacterState.h"
+#include "Component/EQStateComponent.h"
+
+void UEQAnimNotify_ResetCharacterState::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	const AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	UEQStateComponent* StateComponent = Owner->GetComponentByClass<UEQStateComponent>();
+	if (!StateComponent)
+	{
+		return;
+	}
+
+	StateComponent->ClearState();
+}
