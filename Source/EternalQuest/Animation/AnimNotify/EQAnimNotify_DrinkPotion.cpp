ffc4f5bc711ac8a572c84c58bde1172406684456
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-12 18:30:41 +0900

    포션 마시기 노티파이 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_DrinkPotion.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotify_DrinkPotion.h"
+#include "Component/EQInventoryComponent.h"
+
+UEQAnimNotify_DrinkPotion::UEQAnimNotify_DrinkPotion(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+void UEQAnimNotify_DrinkPotion::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	const AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	UEQInventoryComponent* InventoryComponent = Owner->GetComponentByClass<UEQInventoryComponent>();
+	if (!InventoryComponent)
+	{
+		return;
+	}
+
+	InventoryComponent->DrinkPotion();
+}
---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_DrinkPotion.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "EQAnimNotify_DrinkPotion.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimNotify_DrinkPotion : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	explicit UEQAnimNotify_DrinkPotion(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
