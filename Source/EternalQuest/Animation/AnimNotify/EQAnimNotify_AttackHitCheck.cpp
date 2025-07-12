7e2575e3ba4ba37234e861028f73c447ec02bfba
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-12 18:21:40 +0900

    공격 판정 노티파이 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_AttackHitCheck.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotify_AttackHitCheck.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayTag/EQGameplayTag.h"
+
+UEQAnimNotify_AttackHitCheck::UEQAnimNotify_AttackHitCheck()
+{
+	TriggerGameplayTag = EQGameplayTag::Character_Action_AttackHitCheck;
+
+	ComboLevel = 1.0f;
+}
+
+FString UEQAnimNotify_AttackHitCheck::GetNotifyName_Implementation() const
+{
+	return TEXT("AttackHitCheck");
+}
+
+void UEQAnimNotify_AttackHitCheck::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	FGameplayEventData PayloadData;
+	PayloadData.Instigator = Owner;
+	PayloadData.EventMagnitude = ComboLevel;
+
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Owner, TriggerGameplayTag, PayloadData);
+}
---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_AttackHitCheck.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "EQAnimNotify_AttackHitCheck.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimNotify_AttackHitCheck : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	UEQAnimNotify_AttackHitCheck();
+
+protected:
+	virtual FString GetNotifyName_Implementation() const override;
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = GameplayTag, Meta = (AllowPrivateAccess = true))
+	FGameplayTag TriggerGameplayTag;
+
+	UPROPERTY(EditAnywhere, Category = Data, Meta = (AllowPrivateAccess = true))
+	float ComboLevel;
+};
