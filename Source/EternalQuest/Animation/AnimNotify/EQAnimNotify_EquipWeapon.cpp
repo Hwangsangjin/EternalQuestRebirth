2ee1b6e5d636e9b8a1403382de84902f0f5159cd
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-12 18:23:14 +0900

    무기 장착 노티파이 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_EquipWeapon.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotify_EquipWeapon.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Equipment/EQWeapon.h"
+#include "GameplayTag/EQGameplayTag.h"
+#include "Interface/EQCombatCapable.h"
+
+class IEQCombatCapable;
+
+UEQAnimNotify_EquipWeapon::UEQAnimNotify_EquipWeapon(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UEQAnimNotify_EquipWeapon::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	IEQCombatCapable* Interface = Cast<IEQCombatCapable>(Owner);
+	if (!Interface)
+	{
+		return;
+	}
+
+	AEQWeapon* Weapon = Interface->GetWeapon();
+	if (!Weapon)
+	{
+		return;
+	}
+
+	bool bCombatEnabled = Interface->IsCombatEnabled();
+
+	FName SocketName;
+
+	if (MontageActionTag == EQGameplayTag::Character_Action_Equip)
+	{
+		bCombatEnabled = true;
+		SocketName = Weapon->GetEquipSocketName();
+	}
+	else if (MontageActionTag == EQGameplayTag::Character_Action_Unequip)
+	{
+		bCombatEnabled = false;
+		SocketName = Weapon->GetUnequipSocketName();
+	}
+
+	Interface->SetCombatEnabled(bCombatEnabled);
+	Weapon->AttachToOwner(SocketName);
+}
---
+++ Source/EternalQuest/Animation/AnimNotify/EQAnimNotify_EquipWeapon.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "EQAnimNotify_EquipWeapon.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimNotify_EquipWeapon : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	explicit UEQAnimNotify_EquipWeapon(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Tag, Meta = (AllowPrivateAccess = true))
+	FGameplayTag MontageActionTag;
+};
