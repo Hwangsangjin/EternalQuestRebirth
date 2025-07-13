cc83b7a442b8350673830e86b14c951f79f619d0
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-13 12:26:14 +0900

    포션 스폰 노티파이 스테이트 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotifyState/EQAnimNotifyState_PotionSpawn.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotifyState_PotionSpawn.h"
+#include "Component/EQCombatComponent.h"
+#include "Component/EQInventoryComponent.h"
+#include "Equipment/EQShield.h"
+
+UEQAnimNotifyState_PotionSpawn::UEQAnimNotifyState_PotionSpawn(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+void UEQAnimNotifyState_PotionSpawn::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	const AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	UEQInventoryComponent* InventoryComponent = Owner->GetComponentByClass<UEQInventoryComponent>();
+	if (!InventoryComponent)
+	{
+		return;
+	}
+
+	InventoryComponent->SpawnPotion();
+
+	const UEQCombatComponent* CombatComponent = Owner->GetComponentByClass<UEQCombatComponent>();
+	if (!CombatComponent)
+	{
+		return;
+	}
+
+	AEQShield* Shield = CombatComponent->GetShield();
+	if (!Shield)
+	{
+		return;
+	}
+
+	Shield->SetActorHiddenInGame(true);
+}
+
+void UEQAnimNotifyState_PotionSpawn::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	const AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	UEQInventoryComponent* InventoryComponent = Owner->GetComponentByClass<UEQInventoryComponent>();
+	if (!InventoryComponent)
+	{
+		return;
+	}
+
+	InventoryComponent->DespawnPotion();
+
+	const UEQCombatComponent* CombatComponent = Owner->GetComponentByClass<UEQCombatComponent>();
+	if (!CombatComponent)
+	{
+		return;
+	}
+
+	AEQShield* Shield = CombatComponent->GetShield();
+	if (!Shield)
+	{
+		return;
+	}
+
+	Shield->SetActorHiddenInGame(false);
+}
---
+++ Source/EternalQuest/Animation/AnimNotifyState/EQAnimNotifyState_PotionSpawn.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "EQAnimNotifyState_PotionSpawn.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimNotifyState_PotionSpawn : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	explicit UEQAnimNotifyState_PotionSpawn(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
