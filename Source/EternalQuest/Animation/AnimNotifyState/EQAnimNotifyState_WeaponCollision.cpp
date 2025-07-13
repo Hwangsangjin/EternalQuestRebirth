1f5151f0ee1ee6079d009779640068bdfaa63220
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-13 12:25:33 +0900

    무기 충돌 노티파이 스테이트 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotifyState/EQAnimNotifyState_WeaponCollision.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotifyState_WeaponCollision.h"
+#include "Interface/EQCombatCapable.h"
+
+UEQAnimNotifyState_WeaponCollision::UEQAnimNotifyState_WeaponCollision(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+void UEQAnimNotifyState_WeaponCollision::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	IEQCombatCapable* Interface = Cast<IEQCombatCapable>(Owner);
+	if (!Interface)
+	{
+		return;
+	}
+
+	Interface->ActivateWeaponCollision();
+}
+
+void UEQAnimNotifyState_WeaponCollision::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	IEQCombatCapable* Interface = Cast<IEQCombatCapable>(Owner);
+	if (!Interface)
+	{
+		return;
+	}
+
+	Interface->ActivateWeaponCollision();
+}
---
+++ Source/EternalQuest/Animation/AnimNotifyState/EQAnimNotifyState_WeaponCollision.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "EQAnimNotifyState_WeaponCollision.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimNotifyState_WeaponCollision : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	explicit UEQAnimNotifyState_WeaponCollision(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
