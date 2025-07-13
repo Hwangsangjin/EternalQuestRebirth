f815ce486f61c50f788041ccb275b463ae7c43a7
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-13 12:25:55 +0900

    캐릭터 회전 노티파이 스테이트 클래스 추가

---
+++ Source/EternalQuest/Animation/AnimNotifyState/EQAnimNotifyState_CharacterRotate.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAnimNotifyState_CharacterRotate.h"
+#include "Component/EQRotationComponent.h"
+
+UEQAnimNotifyState_CharacterRotate::UEQAnimNotifyState_CharacterRotate()
+{
+}
+
+void UEQAnimNotifyState_CharacterRotate::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	UEQRotationComponent* RotationComponent = MeshComp->GetOwner()->GetComponentByClass<UEQRotationComponent>();
+	if (!RotationComponent)
+	{
+		return;
+	}
+
+	RotationComponent->ToggleShouldRotate(true);
+}
+
+void UEQAnimNotifyState_CharacterRotate::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+	UEQRotationComponent* RotationComponent = MeshComp->GetOwner()->GetComponentByClass<UEQRotationComponent>();
+	if (!RotationComponent)
+	{
+		return;
+	}
+
+	RotationComponent->ToggleShouldRotate(false);
+}
---
+++ Source/EternalQuest/Animation/AnimNotifyState/EQAnimNotifyState_CharacterRotate.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "EQAnimNotifyState_CharacterRotate.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQAnimNotifyState_CharacterRotate : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	UEQAnimNotifyState_CharacterRotate();
+
+protected:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<ACharacter> Owner;
+
+	UPROPERTY()
+	TObjectPtr<AController> Controller;
+
+	FRotator InitialAttackRotation;
+};
