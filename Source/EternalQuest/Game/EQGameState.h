cb468c71a787ca1f786fc8b561bfb1cb3e611c14
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-14 15:52:02 +0900

    게임 스테이트 클래스 추가

---
+++ Source/EternalQuest/Game/EQGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "EQGameState.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	AEQGameState();
+};
