69fd336d3abaa3e10a77aab82fc884598237139e
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-16 12:31:18 +0900

    상호작용 게임플레이 어빌리티 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/EQGameplayAbility_Interact.cpp
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQGameplayAbility_Interact.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilityTask/EQAbilityTask_Trace.h"
+#include "Interface/EQCharacterInteractable.h"
+#include "Physics/EQCollision.h"
+
+UEQGameplayAbility_Interact::UEQGameplayAbility_Interact()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	ObjectType = UEngineTypes::ConvertToObjectType(CCHANNEL_EQPICKUP);
+	TraceRadius = 100.0f;
+}
+
+void UEQGameplayAbility_Interact::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!ActorInfo || !ActorInfo->AvatarActor.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+		return;
+	}
+
+	UEQAbilityTask_Trace* InteractTraceTask = UEQAbilityTask_Trace::CreateTask(this, TargetActorClass);
+	InteractTraceTask->TraceResultDelegate.AddDynamic(this, &ThisClass::OnTraceResultCallback);
+	InteractTraceTask->ReadyForActivation();
+}
+
+void UEQGameplayAbility_Interact::OnTraceResultCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle)
+{
+	if (TargetDataHandle.Num() == 0)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+		return;
+	}
+
+	const FGameplayAbilityTargetData* Data = TargetDataHandle.Get(0);
+	if (!Data)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+		return;
+	}
+
+	const FHitResult* HitResult = Data->GetHitResult();
+	if (!HitResult || !HitResult->GetActor())
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+		return;
+	}
+
+	AActor* HitActor = HitResult->GetActor();
+
+	if (IEQCharacterInteractable* InteractInterface = Cast<IEQCharacterInteractable>(HitActor))
+	{
+		AActor* Interactor = CurrentActorInfo->AvatarActor.Get();
+		InteractInterface->PerformInteract(Interactor);
+	}
+
+	constexpr bool bReplicateEndAbility = true;
+	constexpr bool bWasCancelled = false;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/EternalQuest/GameplayAbility/EQGameplayAbility_Interact.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "EQGameplayAbility_Interact.generated.h"
+
+class AEQTargetActor_Trace;
+
+UCLASS()
+class ETERNALQUEST_API UEQGameplayAbility_Interact : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UEQGameplayAbility_Interact();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+private:
+	UFUNCTION()
+	void OnTraceResultCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
+
+	UPROPERTY(EditAnywhere, Category = GAS, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<AEQTargetActor_Trace> TargetActorClass;
+
+	UPROPERTY(EditAnywhere, Category = Trace, Meta = (AllowPrivateAccess = true))
+	TEnumAsByte<EObjectTypeQuery> ObjectType;
+
+	UPROPERTY(EditAnywhere, Category = Trace, Meta = (AllowPrivateAccess = true))
+	float TraceRadius;
+};
