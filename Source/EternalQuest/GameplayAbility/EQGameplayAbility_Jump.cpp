705e770e42d6d18e12cbc8ce33065e9789757061
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-17 17:37:37 +0900

    점프 게임플레이 어빌리티 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/EQGameplayAbility_Jump.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQGameplayAbility_Jump.h"
+#include "AbilityTask/EQAbilityTask_JumpAndWaitForLanding.h"
+#include "GameFramework/Character.h"
+
+UEQGameplayAbility_Jump::UEQGameplayAbility_Jump()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+bool UEQGameplayAbility_Jump::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	const bool bResult = Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+	if (!bResult)
+	{
+		return false;
+	}
+
+	const ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	return Character && Character->CanJump();
+}
+
+void UEQGameplayAbility_Jump::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UEQAbilityTask_JumpAndWaitForLanding* JumpAndWaitForLandingTask = UEQAbilityTask_JumpAndWaitForLanding::CreateTask(this);
+	JumpAndWaitForLandingTask->JumpAndWaitForLandingDelegate.AddDynamic(this, &ThisClass::OnLandedCallback);
+	JumpAndWaitForLandingTask->ReadyForActivation();
+}
+
+void UEQGameplayAbility_Jump::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	ACharacter* Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
+	Character->StopJumping();
+}
+
+void UEQGameplayAbility_Jump::OnLandedCallback()
+{
+	constexpr bool bReplicateEndAbility = true;
+	constexpr bool bWasCancelled = false;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/EternalQuest/GameplayAbility/EQGameplayAbility_Jump.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "EQGameplayAbility_Jump.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQGameplayAbility_Jump : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UEQGameplayAbility_Jump();
+
+protected:
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+private:
+	UFUNCTION()
+	void OnLandedCallback();
+};
