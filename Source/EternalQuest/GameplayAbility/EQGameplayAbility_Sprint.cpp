9c063a6611c18fc3634c1d74ab607d6e522cba4e
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-18 15:51:06 +0900

    달리기 게임플레이 어빌리티 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/EQGameplayAbility_Sprint.cpp
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQGameplayAbility_Sprint.h"
+#include "AbilitySystemComponent.h"
+#include "EQGameplayAbility_StaminaRegen.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameplayTag/EQGameplayTag.h"
+
+UEQGameplayAbility_Sprint::UEQGameplayAbility_Sprint()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	SprintSpeed = 650.0f;
+	NormalSpeed = 500.0f;
+}
+
+void UEQGameplayAbility_Sprint::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		return;
+	}
+
+	if (ActorInfo->AbilitySystemComponent.IsValid())
+	{
+		const FGameplayAbilitySpec* StaminaRegenSpec = ActorInfo->AbilitySystemComponent->FindAbilitySpecFromClass(UEQGameplayAbility_StaminaRegen::StaticClass());
+		if (StaminaRegenSpec && StaminaRegenSpec->Ability)
+		{
+			ActorInfo->AbilitySystemComponent->CancelAbility(StaminaRegenSpec->Ability);
+		}
+	}
+
+	if (!SprintStaminaDrainEffect)
+	{
+		return;
+	}
+
+	const ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	if (!Character)
+	{
+		return;
+	}
+
+	Character->GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+
+	if (ActorInfo->AbilitySystemComponent.IsValid())
+	{
+		StaminaDrainHandle = ActorInfo->AbilitySystemComponent->ApplyGameplayEffectToSelf(SprintStaminaDrainEffect->GetDefaultObject<UGameplayEffect>(), 1.0f, ActorInfo->AbilitySystemComponent->MakeEffectContext());
+	}
+}
+
+void UEQGameplayAbility_Sprint::InputReleased(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	constexpr bool bReplicateEndAbility = true;
+	constexpr bool bWasCancelled = false;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+bool UEQGameplayAbility_Sprint::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	const ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	if (!Character)
+	{
+		return false;
+	}
+
+	const UCharacterMovementComponent* MovementComponent = Character->GetCharacterMovement();
+	if (!MovementComponent)
+	{
+		return false;
+	}
+
+	return MovementComponent->IsMovingOnGround() && !MovementComponent->Velocity.IsNearlyZero();
+}
+
+void UEQGameplayAbility_Sprint::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	const ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
+	if (!Character)
+	{
+		return;
+	}
+
+	Character->GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+
+	if (ActorInfo->AbilitySystemComponent.IsValid())
+	{
+		ActorInfo->AbilitySystemComponent->RemoveLooseGameplayTag(EQGameplayTag::Character_State_Sprinting);
+
+		if (StaminaDrainHandle.IsValid())
+		{
+			ActorInfo->AbilitySystemComponent->RemoveActiveGameplayEffect(StaminaDrainHandle);
+			StaminaDrainHandle.Invalidate();
+		}
+
+		const FGameplayAbilitySpec* Spec = ActorInfo->AbilitySystemComponent->FindAbilitySpecFromClass(UEQGameplayAbility_StaminaRegen::StaticClass());
+		if (Spec)
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec->Handle);
+		}
+	}
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/EternalQuest/GameplayAbility/EQGameplayAbility_Sprint.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "EQGameplayAbility_Sprint.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQGameplayAbility_Sprint : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UEQGameplayAbility_Sprint();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = Sprint, Meta = (AllowPrivateAccess = true))
+	TSubclassOf<UGameplayEffect> SprintStaminaDrainEffect;
+
+	UPROPERTY(EditAnywhere, Category = Sprint, Meta = (AllowPrivateAccess = true))
+	float SprintSpeed;
+
+	UPROPERTY(EditAnywhere, Category = Sprint, Meta = (AllowPrivateAccess = true))
+	float NormalSpeed;
+
+	FActiveGameplayEffectHandle StaminaDrainHandle;
+	FGameplayAbilitySpecHandle StaminaRegenAbilityHandle;
+};
