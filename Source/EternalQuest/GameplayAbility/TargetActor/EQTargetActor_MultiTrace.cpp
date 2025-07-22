e7cbf0a882bad297f9b12c8031b0de94413299c9
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-22 17:22:42 +0900

    멀티 트레이스 타겟 액터 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/TargetActor/EQTargetActor_MultiTrace.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQTargetActor_MultiTrace.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Attribute/EQCharacterSkillAttributeSet.h"
+#include "Engine/OverlapResult.h"
+#include "GameFramework/Character.h"
+#include "Physics/EQCollision.h"
+
+AEQTargetActor_MultiTrace::AEQTargetActor_MultiTrace()
+{
+}
+
+FGameplayAbilityTargetDataHandle AEQTargetActor_MultiTrace::MakeTargetData() const
+{
+	const ACharacter* Character = CastChecked<ACharacter>(SourceActor);
+
+	const UAbilitySystemComponent* AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(SourceActor);
+	if (!AbilitySystemComponent)
+	{
+		return FGameplayAbilityTargetDataHandle();
+	}
+
+	const UEQCharacterSkillAttributeSet* SkillAttribute = AbilitySystemComponent->GetSet<UEQCharacterSkillAttributeSet>();
+	if (!SkillAttribute)
+	{
+		return FGameplayAbilityTargetDataHandle();
+	}
+
+	TArray<FOverlapResult> Overlaps;
+	const FVector Origin = Character->GetActorLocation();
+	const float SkillRadius = SkillAttribute->GetSkillRange();
+	const FCollisionQueryParams Params(SCENE_QUERY_STAT(MultiTrace), false, Character);
+	GetWorld()->OverlapMultiByChannel(Overlaps, Origin, FQuat::Identity, CCHANNEL_EQACTION, FCollisionShape::MakeSphere(SkillRadius), Params);
+
+	TArray<TWeakObjectPtr<AActor>> HitActors;
+
+	for (const FOverlapResult& Overlap : Overlaps)
+	{
+		AActor* HitActor = Overlap.OverlapObjectHandle.FetchActor<AActor>();
+		if (!HitActor || HitActors.Contains(HitActor))
+		{
+			continue;
+		}
+
+		HitActors.Add(HitActor);
+	}
+
+	FGameplayAbilityTargetData_ActorArray* TargetData = new FGameplayAbilityTargetData_ActorArray;
+	TargetData->SetActors(HitActors);
+
+#if ENABLE_DRAW_DEBUG
+	if (bDebugEnabled)
+	{
+		const FColor DrawColor = HitActors.Num() > 0 ? FColor::Green : FColor::Red;
+		DrawDebugSphere(GetWorld(), Origin, SkillRadius, 16, DrawColor, false, 5.0f);
+	}
+#endif
+
+	return FGameplayAbilityTargetDataHandle(TargetData);
+}
---
+++ Source/EternalQuest/GameplayAbility/TargetActor/EQTargetActor_MultiTrace.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQTargetActor_Trace.h"
+#include "EQTargetActor_MultiTrace.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQTargetActor_MultiTrace : public AEQTargetActor_Trace
+{
+	GENERATED_BODY()
+
+public:
+	AEQTargetActor_MultiTrace();
+
+protected:
+	virtual FGameplayAbilityTargetDataHandle MakeTargetData() const override;
+};
