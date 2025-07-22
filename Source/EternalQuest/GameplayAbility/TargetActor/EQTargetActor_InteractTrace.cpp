03060813b2cb78d686a3cfde52ce7e8a699b87cd
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-22 17:23:01 +0900

    상호작용 트레이스 타겟 액터 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/TargetActor/EQTargetActor_InteractTrace.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQTargetActor_InteractTrace.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Physics/EQCollision.h"
+
+AEQTargetActor_InteractTrace::AEQTargetActor_InteractTrace()
+{
+	ObjectType = UEngineTypes::ConvertToObjectType(CCHANNEL_EQPICKUP);
+	TraceRadius = 100.0f;
+}
+
+void AEQTargetActor_InteractTrace::StartTargeting(UGameplayAbility* Ability)
+{
+	Super::StartTargeting(Ability);
+
+	SourceActor = Ability->GetCurrentActorInfo()->AvatarActor.Get();
+}
+
+void AEQTargetActor_InteractTrace::ConfirmTargetingAndContinue()
+{
+	if (!SourceActor)
+	{
+		return;
+	}
+
+	const FGameplayAbilityTargetDataHandle TargetDataHandle = MakeTargetData();
+	TargetDataReadyDelegate.Broadcast(TargetDataHandle);
+}
+
+FGameplayAbilityTargetDataHandle AEQTargetActor_InteractTrace::MakeTargetData() const
+{
+	const ACharacter* Character = CastChecked<ACharacter>(SourceActor);
+
+	FGameplayAbilityTargetDataHandle TargetDataHandle;
+
+	const FVector Start = Character->GetActorLocation();
+	const FVector End = Start;
+
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(ObjectType);
+
+	const TArray<AActor*> ActorsToIgnore;
+	FHitResult HitResult;
+
+	const bool bHit = UKismetSystemLibrary::SphereTraceSingleForObjects(this, Start, End, TraceRadius, ObjectTypes, false, ActorsToIgnore, EDrawDebugTrace::ForDuration, HitResult, true);
+	if (!bHit || !HitResult.GetActor())
+	{
+		return TargetDataHandle;
+	}
+
+	FGameplayAbilityTargetData_SingleTargetHit* TargetData = new FGameplayAbilityTargetData_SingleTargetHit(HitResult);
+	TargetDataHandle.Add(TargetData);
+
+	return TargetDataHandle;
+}
---
+++ Source/EternalQuest/GameplayAbility/TargetActor/EQTargetActor_InteractTrace.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EQTargetActor_Trace.h"
+#include "EQTargetActor_InteractTrace.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API AEQTargetActor_InteractTrace : public AEQTargetActor_Trace
+{
+	GENERATED_BODY()
+
+public:
+	AEQTargetActor_InteractTrace();
+
+	virtual void StartTargeting(UGameplayAbility* Ability) override;
+	virtual void ConfirmTargetingAndContinue() override;
+
+protected:
+	virtual FGameplayAbilityTargetDataHandle MakeTargetData() const override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Trace, Meta = (AllowPrivateAccess = true))
+	TEnumAsByte<EObjectTypeQuery> ObjectType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Trace, Meta = (AllowPrivateAccess = true))
+	float TraceRadius;
+};
