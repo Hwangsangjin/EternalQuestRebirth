ec8124e2db50a8be96ada42eebd3114ca8c0d3f8
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-23 12:05:05 +0900

    점프 게임플레이 어빌리티 태스크 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/AbilityTask/EQAbilityTask_JumpAndWaitForLanding.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAbilityTask_JumpAndWaitForLanding.h"
+#include "GameFramework/Character.h"
+
+UEQAbilityTask_JumpAndWaitForLanding::UEQAbilityTask_JumpAndWaitForLanding()
+{
+}
+
+UEQAbilityTask_JumpAndWaitForLanding* UEQAbilityTask_JumpAndWaitForLanding::CreateTask(UGameplayAbility* OwningAbility)
+{
+	UEQAbilityTask_JumpAndWaitForLanding* NewTask = NewAbilityTask<UEQAbilityTask_JumpAndWaitForLanding>(OwningAbility);
+	return NewTask;
+}
+
+void UEQAbilityTask_JumpAndWaitForLanding::Activate()
+{
+	Super::Activate();
+
+	ACharacter* Character = CastChecked<ACharacter>(GetAvatarActor());
+	Character->LandedDelegate.AddDynamic(this, &ThisClass::OnLandedCallback);
+	Character->Jump();
+
+	SetWaitingOnAvatar();
+}
+
+void UEQAbilityTask_JumpAndWaitForLanding::OnDestroy(bool bInOwnerFinished)
+{
+	ACharacter* Character = CastChecked<ACharacter>(GetAvatarActor());
+	Character->LandedDelegate.RemoveDynamic(this, &ThisClass::OnLandedCallback);
+
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UEQAbilityTask_JumpAndWaitForLanding::OnLandedCallback(const FHitResult& Hit)
+{
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		JumpAndWaitForLandingDelegate.Broadcast();
+	}
+}
---
+++ Source/EternalQuest/GameplayAbility/AbilityTask/EQAbilityTask_JumpAndWaitForLanding.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "EQAbilityTask_JumpAndWaitForLanding.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FJumpAndWaitForLandingDelegate);
+
+UCLASS()
+class ETERNALQUEST_API UEQAbilityTask_JumpAndWaitForLanding : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UEQAbilityTask_JumpAndWaitForLanding();
+
+	static UEQAbilityTask_JumpAndWaitForLanding* CreateTask(UGameplayAbility* OwningAbility);
+
+	FJumpAndWaitForLandingDelegate JumpAndWaitForLandingDelegate;
+
+protected:
+	virtual void Activate() override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+
+	UFUNCTION()
+	void OnLandedCallback(const FHitResult& Hit);
+};
