87d8b0fcbe283b2af3fc06cc9f05cc1ebb35c228
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-24 12:33:42 +0900

    트레이스 게임플레이 어빌리티 태스크 클래스 추가

---
+++ Source/EternalQuest/GameplayAbility/AbilityTask/EQAbilityTask_Trace.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAbilityTask_Trace.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayAbility/TargetActor/EQTargetActor_Trace.h"
+
+UEQAbilityTask_Trace::UEQAbilityTask_Trace()
+{
+}
+
+UEQAbilityTask_Trace* UEQAbilityTask_Trace::CreateTask(UGameplayAbility* OwningAbility, const TSubclassOf<AEQTargetActor_Trace>& TargetActorClass)
+{
+	UEQAbilityTask_Trace* NewTask = NewAbilityTask<UEQAbilityTask_Trace>(OwningAbility);
+	NewTask->TargetActorClass = TargetActorClass;
+	return NewTask;
+}
+
+void UEQAbilityTask_Trace::Activate()
+{
+	Super::Activate();
+
+	SpawnAndInitializeTargetActor();
+	FinalizeTargetActor();
+
+	SetWaitingOnAvatar();
+}
+
+void UEQAbilityTask_Trace::OnDestroy(bool bInOwnerFinished)
+{
+	if (SpawnedTargetActor)
+	{
+		SpawnedTargetActor->Destroy();
+	}
+
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UEQAbilityTask_Trace::SpawnAndInitializeTargetActor()
+{
+	if (!Ability || !TargetActorClass)
+	{
+		EndTask();
+		return;
+	}
+
+	SpawnedTargetActor = Cast<AEQTargetActor_Trace>(Ability->GetWorld()->SpawnActorDeferred<AGameplayAbilityTargetActor>(TargetActorClass, FTransform::Identity, nullptr, nullptr, ESpawnActorCollisionHandlingMethod::AlwaysSpawn));
+	if (!SpawnedTargetActor)
+	{
+		EndTask();
+		return;
+	}
+
+	SpawnedTargetActor->SetDebugEnabled(true);
+	SpawnedTargetActor->TargetDataReadyDelegate.AddUObject(this, &ThisClass::OnTargetDataReadyCallback);
+}
+
+void UEQAbilityTask_Trace::FinalizeTargetActor()
+{
+	UAbilitySystemComponent* AbilitySystemComponentPtr = AbilitySystemComponent.Get();
+	if (!AbilitySystemComponentPtr)
+	{
+		EndTask();
+		return;
+	}
+
+	const FTransform SpawnTransform = AbilitySystemComponentPtr->GetAvatarActor()->GetTransform();
+	SpawnedTargetActor->FinishSpawning(SpawnTransform);
+
+	AbilitySystemComponentPtr->SpawnedTargetActors.Push(SpawnedTargetActor);
+	SpawnedTargetActor->StartTargeting(Ability);
+	SpawnedTargetActor->ConfirmTargeting();
+}
+
+void UEQAbilityTask_Trace::OnTargetDataReadyCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle)
+{
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		TraceResultDelegate.Broadcast(TargetDataHandle);
+	}
+
+	EndTask();
+}
---
+++ Source/EternalQuest/GameplayAbility/AbilityTask/EQAbilityTask_Trace.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "EQAbilityTask_Trace.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FTraceResultDelegate, const FGameplayAbilityTargetDataHandle&, TargetDataHandle);
+
+class AEQTargetActor_Trace;
+
+UCLASS()
+class ETERNALQUEST_API UEQAbilityTask_Trace : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UEQAbilityTask_Trace();
+
+	UFUNCTION()
+	static UEQAbilityTask_Trace* CreateTask(UGameplayAbility* OwningAbility, const TSubclassOf<AEQTargetActor_Trace>& TargetActorClass);
+
+	virtual void Activate() override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+
+	void SpawnAndInitializeTargetActor();
+	void FinalizeTargetActor();
+
+	void OnTargetDataReadyCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
+
+	UPROPERTY()
+	FTraceResultDelegate TraceResultDelegate;
+
+private:
+	UPROPERTY()
+	TSubclassOf<AEQTargetActor_Trace> TargetActorClass;
+
+	UPROPERTY()
+	TObjectPtr<AEQTargetActor_Trace> SpawnedTargetActor;
+};
