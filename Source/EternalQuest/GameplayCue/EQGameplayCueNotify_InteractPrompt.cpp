8db84b7c54e354b08a6816c5d382529d0c4f783a
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-25 11:27:50 +0900

    상호작용 프롬프트 게임플레이 큐 노티파이 클래스 추가

---
+++ Source/EternalQuest/GameplayCue/EQGameplayCueNotify_InteractPrompt.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQGameplayCueNotify_InteractPrompt.h"
+#include "GameplayTag/EQGameplayTag.h"
+#include "Player/EQPlayerController.h"
+#include "UI/EQHUDWidget.h"
+
+UEQGameplayCueNotify_InteractPrompt::UEQGameplayCueNotify_InteractPrompt()
+{
+}
+
+bool UEQGameplayCueNotify_InteractPrompt::OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) const
+{
+	const APawn* Pawn = Cast<APawn>(MyTarget);
+	const APlayerController* PlayerController = Pawn ? Cast<APlayerController>(Pawn->GetController()) : nullptr;
+	if (!PlayerController)
+	{
+		return false;
+	}
+
+	const AEQPlayerController* EQPlayerController = Cast<AEQPlayerController>(PlayerController);
+	if (!EQPlayerController)
+	{
+		return false;
+	}
+
+	const UEQHUDWidget* HUDWidget = EQPlayerController->GetHUDWidget();
+	if (!HUDWidget)
+	{
+		return false;
+	}
+
+	UEQUserWidget* InteractPromptWidget = HUDWidget->GetInteractPromptWidget();
+	if (!InteractPromptWidget)
+	{
+		return false;
+	}
+
+	if (GameplayCueTag == EQGameplayTag::GameplayCue_InteractPrompt_Show)
+	{
+		InteractPromptWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+	else if (GameplayCueTag == EQGameplayTag::GameplayCue_InteractPrompt_Hide)
+	{
+		InteractPromptWidget->SetVisibility(ESlateVisibility::Hidden);
+	}
+
+	return true;
+}
---
+++ Source/EternalQuest/GameplayCue/EQGameplayCueNotify_InteractPrompt.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Static.h"
+#include "EQGameplayCueNotify_InteractPrompt.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQGameplayCueNotify_InteractPrompt : public UGameplayCueNotify_Static
+{
+	GENERATED_BODY()
+
+public:
+	UEQGameplayCueNotify_InteractPrompt();
+
+protected:
+	virtual bool OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) const override;
+};
