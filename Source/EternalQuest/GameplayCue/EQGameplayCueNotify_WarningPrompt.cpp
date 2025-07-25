d642257dd024170e1eed75f07ec0a6bb9bf453a1
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-25 11:28:04 +0900

    경고 프롬프트 게임플레이 큐 노티파이 클래스 추가

---
+++ Source/EternalQuest/GameplayCue/EQGameplayCueNotify_WarningPrompt.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQGameplayCueNotify_WarningPrompt.h"
+#include "Player/EQPlayerController.h"
+#include "UI/EQHUDWidget.h"
+
+UEQGameplayCueNotify_WarningPrompt::UEQGameplayCueNotify_WarningPrompt()
+{
+}
+
+bool UEQGameplayCueNotify_WarningPrompt::OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) const
+{
+	const APawn* Pawn = Cast<APawn>(MyTarget);
+	const APlayerController* PlayerController = Pawn ? Cast<APlayerController>(Pawn->GetController()) : nullptr;
+	if (!PlayerController)
+	{
+		return false;
+	}
+
+	const AEQPlayerController* EQPlayerController = Cast<AEQPlayerController>(PlayerController);
+	if (!EQPlayerController)
+	{
+		return false;
+	}
+
+	const UEQHUDWidget* HUDWidget = EQPlayerController->GetHUDWidget();
+	if (!HUDWidget)
+	{
+		return false;
+	}
+
+	UEQUserWidget* WarningPromptWidget = HUDWidget->GetWarningPromptWidget();
+	if (!WarningPromptWidget)
+	{
+		return false;
+	}
+
+	WarningPromptWidget->SetVisibility(ESlateVisibility::Visible);
+
+	TWeakObjectPtr WeakWarningPromptWidget = WarningPromptWidget;
+
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, [WeakWarningPromptWidget]()
+	{
+		if (WeakWarningPromptWidget.IsValid())
+		{
+			WeakWarningPromptWidget->SetVisibility(ESlateVisibility::Hidden);
+		}
+	}, 1.5f, false);
+
+	return true;
+}
---
+++ Source/EternalQuest/GameplayCue/EQGameplayCueNotify_WarningPrompt.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Static.h"
+#include "EQGameplayCueNotify_WarningPrompt.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQGameplayCueNotify_WarningPrompt : public UGameplayCueNotify_Static
+{
+	GENERATED_BODY()
+
+public:
+	UEQGameplayCueNotify_WarningPrompt();
+
+protected:
+	virtual bool OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters) const override;
+};
