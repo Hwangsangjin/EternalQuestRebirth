5c6988524772d0722c6183748b78398b1ec08c42
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-26 10:59:58 +0900

    공격 데미지 게임플레이 이펙트 클래스 추가

---
+++ Source/EternalQuest/GameplayEffect/EQGameplayEffect_AttackDamage.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQGameplayEffect_AttackDamage.h"
+#include "Attribute/EQCharacterAttributeSet.h"
+#include "Character/EQBaseCharacter.h"
+#include "Component/EQStateComponent.h"
+#include "GameplayTag/EQGameplayTag.h"
+
+UEQGameplayEffect_AttackDamage::UEQGameplayEffect_AttackDamage()
+{
+}
+
+void UEQGameplayEffect_AttackDamage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+	AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+	if (!TargetActor)
+	{
+		return;
+	}
+
+	AEQBaseCharacter* TargetCharacter = Cast<AEQBaseCharacter>(TargetActor);
+	if (!TargetCharacter)
+	{
+		return;
+	}
+}
---
+++ Source/EternalQuest/GameplayEffect/EQGameplayEffect_AttackDamage.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "EQGameplayEffect_AttackDamage.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQGameplayEffect_AttackDamage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UEQGameplayEffect_AttackDamage();
+
+protected:
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
