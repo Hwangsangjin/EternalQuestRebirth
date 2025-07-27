9d7ee9a36abeb7424c6d3c059bd9216266b20de5
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-27 15:10:51 +0900

    AI 컨트롤러 클래스 추가

---
+++ Source/EternalQuest/AI/EQAIController.cpp
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Character/EQNonPlayerCharacter.h"
+#include "Component/EQRotationComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "Perception/AIPerceptionComponent.h"
+
+AEQAIController::AEQAIController()
+{
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
+}
+
+void AEQAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	ControlledCharacter = Cast<AEQNonPlayerCharacter>(InPawn);
+
+	RunBehaviorTree(BehaviorTreeAsset);
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ThisClass::UpdateTarget, 0.1f, true);
+}
+
+void AEQAIController::OnUnPossess()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+
+	Super::OnUnPossess();
+
+	ControlledCharacter = nullptr;
+}
+
+void AEQAIController::UpdateTarget() const
+{
+	TArray<AActor*> Actors;
+
+	AIPerceptionComponent->GetKnownPerceivedActors(nullptr, Actors);
+
+	AEQBaseCharacter* PlayerCharacter = Cast<AEQBaseCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+
+	if (Actors.Contains(PlayerCharacter))
+	{
+		if (PlayerCharacter->IsDead())
+		{
+			SetTargetActor(nullptr);
+		}
+		else
+		{
+			SetTargetActor(PlayerCharacter);
+		}
+	}
+	else
+	{
+		SetTargetActor(nullptr);
+	}
+}
+
+void AEQAIController::SetTargetActor(AActor* InTargetActor) const
+{
+	if (!Blackboard)
+	{
+		return;
+	}
+
+	Blackboard->SetValueAsObject(FName("TargetActor"), InTargetActor);
+
+	UEQRotationComponent* RotationComponent = ControlledCharacter->GetComponentByClass<UEQRotationComponent>();
+	if (!RotationComponent)
+	{
+		return;
+	}
+
+	RotationComponent->SetTargetActor(InTargetActor);
+}
---
+++ Source/EternalQuest/AI/EQAIController.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "EQAIController.generated.h"
+
+class AEQNonPlayerCharacter;
+
+UCLASS()
+class ETERNALQUEST_API AEQAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AEQAIController();
+
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+
+	void UpdateTarget() const;
+	void SetTargetActor(AActor* InTargetActor) const;
+
+private:
+	UPROPERTY(EditAnywhere, Category = AI, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UBehaviorTree> BehaviorTreeAsset;
+
+	UPROPERTY(VisibleAnywhere, Category = AI, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = AI, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<AEQNonPlayerCharacter> ControlledCharacter;
+
+	FTimerHandle TimerHandle;
+};
