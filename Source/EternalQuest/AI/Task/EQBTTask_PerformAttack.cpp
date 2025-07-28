94f2ab8b26e054c7b30dd8595cc45832337edc96
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-28 13:51:51 +0900

    공격 수행 태스크 클래스 추가

---
+++ Source/EternalQuest/AI/Task/EQBTTask_PerformAttack.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBTTask_PerformAttack.h"
+#include "AIController.h"
+#include "Component/EQStateComponent.h"
+#include "Interface/EQCombatCapable.h"
+
+EBTNodeResult::Type UEQBTTask_PerformAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	APawn* ControlledPawn = Cast<APawn>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!ControlledPawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	IEQCombatCapable* CombatInterface = Cast<IEQCombatCapable>(ControlledPawn);
+	if (!CombatInterface)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	TWeakObjectPtr<UBehaviorTreeComponent> WeakOwnerComp = &OwnerComp;
+
+	FOnMontageEnded OnMontageEnded;
+	OnMontageEnded.BindLambda([this, ControlledPawn, WeakOwnerComp](UAnimMontage* Montage, bool bInterrupted)
+	{
+		if (!WeakOwnerComp.IsValid())
+		{
+			return;
+		}
+
+		if (UEQStateComponent* StateComponent = ControlledPawn->GetComponentByClass<UEQStateComponent>())
+		{
+			StateComponent->ClearState();
+		}
+
+		FinishLatentTask(*WeakOwnerComp.Get(), EBTNodeResult::Succeeded);
+	});
+
+	CombatInterface->PerformAttack(AttackTypeTag, OnMontageEnded);
+	return EBTNodeResult::InProgress;
+}
---
+++ Source/EternalQuest/AI/Task/EQBTTask_PerformAttack.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "EQBTTask_PerformAttack.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQBTTask_PerformAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	FGameplayTag AttackTypeTag;
+};
