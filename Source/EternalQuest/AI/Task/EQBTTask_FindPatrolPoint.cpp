1fb5d67f1d96f7517850dfd51b653f97a13d2463
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-28 13:50:34 +0900

    순찰 지점 찾기 태스크 클래스 추가

---
+++ Source/EternalQuest/AI/Task/EQBTTask_FindPatrolPoint.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBTTask_FindPatrolPoint.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Character/EQNonPlayerCharacter.h"
+#include "Engine/TargetPoint.h"
+
+UEQBTTask_FindPatrolPoint::UEQBTTask_FindPatrolPoint()
+{
+}
+
+EBTNodeResult::Type UEQBTTask_FindPatrolPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (!ControlledPawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	AEQNonPlayerCharacter* Character = Cast<AEQNonPlayerCharacter>(ControlledPawn);
+	if (!Character)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsVector(BlackboardLocation.SelectedKeyName, Character->GetPatrolPoint()->GetActorLocation());
+	Character->IncrementPatrolIndex();
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/EternalQuest/AI/Task/EQBTTask_FindPatrolPoint.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "EQBTTask_FindPatrolPoint.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQBTTask_FindPatrolPoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UEQBTTask_FindPatrolPoint();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	FBlackboardKeySelector BlackboardLocation;
+};
