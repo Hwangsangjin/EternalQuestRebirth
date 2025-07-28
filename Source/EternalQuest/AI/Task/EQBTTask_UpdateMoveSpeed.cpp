56644b533af223f0e26247b16b103306c2d13f08
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-28 13:51:27 +0900

    이동 속도 갱신 태스크 클래스 추가

---
+++ Source/EternalQuest/AI/Task/EQBTTask_UpdateMoveSpeed.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBTTask_UpdateMoveSpeed.h"
+#include "AIController.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UEQBTTask_UpdateMoveSpeed::UEQBTTask_UpdateMoveSpeed()
+{
+	MaxWalkSpeed = 200.0f;
+}
+
+EBTNodeResult::Type UEQBTTask_UpdateMoveSpeed::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const APawn* ControlledPawn = Cast<APawn>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!ControlledPawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UCharacterMovementComponent* CharacterMovementComponent = ControlledPawn->GetComponentByClass<UCharacterMovementComponent>();
+	if (!CharacterMovementComponent)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	CharacterMovementComponent->MaxWalkSpeed = MaxWalkSpeed;
+	return EBTNodeResult::Succeeded;
+}
