d1725114a8081a2ed59ffacd0c787a1c6fdf9544
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-29 14:49:23 +0900

    확률 확인 데코레이터 클래스 추가

---
+++ Source/EternalQuest/AI/Decorator/EQBTDecorator_ChanceCheck.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBTDecorator_ChanceCheck.h"
+
+UEQBTDecorator_ChanceCheck::UEQBTDecorator_ChanceCheck()
+{
+}
+
+bool UEQBTDecorator_ChanceCheck::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	return ChanceRate > FMath::RandRange(1.0f, 100.0f);
+}
---
+++ Source/EternalQuest/AI/Decorator/EQBTDecorator_ChanceCheck.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "EQBTDecorator_ChanceCheck.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQBTDecorator_ChanceCheck : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UEQBTDecorator_ChanceCheck();
+
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	float ChanceRate;
+};
