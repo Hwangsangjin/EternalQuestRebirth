f4df7113bcd3d2b77519b611ecfb2b35301e4e27
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-29 14:48:29 +0900

    거리 확인 데코레이터 클래스 추가

---
+++ Source/EternalQuest/AI/Decorator/EQBTDecorator_InRangeCheck.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBTDecorator_InRangeCheck.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UEQBTDecorator_InRangeCheck::UEQBTDecorator_InRangeCheck()
+{
+}
+
+bool UEQBTDecorator_InRangeCheck::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (!ControlledPawn)
+	{
+		return false;
+	}
+
+	const AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(TargetBlackboardKey.SelectedKeyName));
+	if (!TargetActor)
+	{
+		return false;
+	}
+
+	const float Distance = ControlledPawn->GetDistanceTo(TargetActor);
+	return UKismetMathLibrary::InRange_FloatFloat(Distance, MinRange, MaxRange);
+}
---
+++ Source/EternalQuest/AI/Decorator/EQBTDecorator_InRangeCheck.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "EQBTDecorator_InRangeCheck.generated.h"
+
+UCLASS()
+class ETERNALQUEST_API UEQBTDecorator_InRangeCheck : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UEQBTDecorator_InRangeCheck();
+
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	FBlackboardKeySelector TargetBlackboardKey;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	float MinRange;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	float MaxRange;
+};
