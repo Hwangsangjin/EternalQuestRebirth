06f8ff99e40b3edb90dece1bd90259cc8749946f
Hwangsangjin <Hwangsangjin88@gmail.com>
2025-07-30 10:50:11 +0900

    행동 선택 서비스 클래스 추가

---
+++ Source/EternalQuest/AI/Service/EQBTService_SelectBehavior.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EQBTService_SelectBehavior.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Character/EQNonPlayerCharacter.h"
+
+UEQBTService_SelectBehavior::UEQBTService_SelectBehavior()
+{
+	INIT_SERVICE_NODE_NOTIFY_FLAGS();
+}
+
+void UEQBTService_SelectBehavior::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::OnBecomeRelevant(OwnerComp, NodeMemory);
+
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (!ControlledPawn)
+	{
+		return;
+	}
+
+	ControlledEnemy = Cast<AEQNonPlayerCharacter>(ControlledPawn);
+}
+
+void UEQBTService_SelectBehavior::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UpdateBehavior(OwnerComp.GetBlackboardComponent());
+}
+
+void UEQBTService_SelectBehavior::SetBehaviorKey(UBlackboardComponent* BlackboardComponent, EAIBehavior Behavior) const
+{
+	BlackboardComponent->SetValueAsEnum(BehaviorKey.SelectedKeyName, static_cast<int32>(Behavior));
+}
+
+void UEQBTService_SelectBehavior::UpdateBehavior(UBlackboardComponent* BlackboardComponent) const
+{
+	check(BlackboardComponent);
+	check(ControlledEnemy);
+
+	AActor* TargetActor = Cast<AActor>(BlackboardComponent->GetValueAsObject(TargetKey.SelectedKeyName));
+	if (TargetActor)
+	{
+		const float Distance = TargetActor->GetDistanceTo(ControlledEnemy);
+
+		if (Distance <= AttackRangeDistance)
+		{
+			SetBehaviorKey(BlackboardComponent, EAIBehavior::MeleeAttack);
+		}
+		else
+		{
+			SetBehaviorKey(BlackboardComponent, EAIBehavior::Approach);
+		}
+	}
+	else
+	{
+		if (ControlledEnemy->GetPatrolPoint())
+		{
+			SetBehaviorKey(BlackboardComponent, EAIBehavior::Patrol);
+		}
+		else
+		{
+			SetBehaviorKey(BlackboardComponent, EAIBehavior::Idle);
+		}
+	}
+}
---
+++ Source/EternalQuest/AI/Service/EQBTService_SelectBehavior.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "EQBTService_SelectBehavior.generated.h"
+
+enum class EAIBehavior : uint8;
+class AEQNonPlayerCharacter;
+
+UCLASS()
+class ETERNALQUEST_API UEQBTService_SelectBehavior : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+	UEQBTService_SelectBehavior();
+
+protected:
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
+	void SetBehaviorKey(UBlackboardComponent* BlackboardComponent, EAIBehavior Behavior) const;
+	void UpdateBehavior(UBlackboardComponent* BlackboardComponent) const;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = AI, Meta = (AllowPrivateAccess = true))
+	TObjectPtr<AEQNonPlayerCharacter> ControlledEnemy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	FBlackboardKeySelector TargetKey;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	FBlackboardKeySelector BehaviorKey;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, Meta = (AllowPrivateAccess = true))
+	float AttackRangeDistance;
+};
